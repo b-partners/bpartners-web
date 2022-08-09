1539879eea9afb0572e6a40672aa97b04066284b
kirioka <hei.amour.2@gmail.com>
2022-08-09 21:11:24 +0300

    style: refactor ByStatusFeelList

--- src/operations/fees/ByStatusFeeList.js
+++ src/operations/fees/ByStatusFeeList.js
@@ -1,19 +1,22 @@
-import { List, Datagrid, TextField, DateField, FunctionField, ShowButton } from 'react-admin'
+import React from 'react';
+import {
+  List, Datagrid, TextField, DateField, FunctionField, ShowButton,
+} from 'react-admin';
 
-import rowStyle from './byStatusRowStyle'
-import { prettyPrintMoney } from '../utils/money'
+import rowStyle from './byStatusRowStyle';
+import { prettyPrintMoney } from '../utils/money.ts';
 
-import { maxPageSize } from '../../providers/dataProvider'
+import { maxPageSize } from '../../providers/dataProvider.ts';
 
-const ByStatusFeeList = ({ status, ...props }) => {
-  status = status ? status : 'LATE'
+function ByStatusFeeList({ status, ...props }) {
+  status = status ? status : 'LATE';
   return (
     <List
       {...props}
       title={`Frais de statut ${status}`}
-      resource='fees'
-      basePath={`/fees`}
-      label='Frais'
+      resource="fees"
+      basePath="/fees"
+      label="Frais"
       actions={null}
       filterDefaultValues={{ status: status }}
       bulkActionButtons={false}
@@ -21,14 +24,18 @@ const ByStatusFeeList = ({ status, ...props }) => {
       perPage={maxPageSize}
     >
       <Datagrid rowClick={id => `/fees/${id}/show`} rowStyle={rowStyle}>
-        <DateField source='due_datetime' label='Date limite' />
-        <TextField source='comment' label='Commentaire' />
-        <FunctionField label='Reste à payer' render={record => prettyPrintMoney(record.remaining_amount)} textAlign='right' />
-        <DateField source='creation_datetime' label='Date de création' />
-        <ShowButton basePath='/fees' />
+        <DateField source="due_datetime" label="Date limite" />
+        <TextField source="comment" label="Commentaire" />
+        <FunctionField
+          label="Reste à payer"
+          render={(record) => prettyPrintMoney(record.remaining_amount)}
+          textAlign="right"
+        />
+        <DateField source="creation_datetime" label="Date de création" />
+        <ShowButton basePath="/fees" />
       </Datagrid>
     </List>
-  )
+  );
 }
 
-export default ByStatusFeeList
+export default ByStatusFeeList;
