a1de885d9b41bdbbdfd6767fd821f518ac74126b
kirioka <hei.amour.2@gmail.com>
2022-08-09 16:02:40 +0300

    fix: rename files

---
+++ src/menu/HaMenu.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ManagerMenu from './ManagerMenu';
+
+function HaMenu() {
+  return <ManagerMenu />;
+}
+
+export default HaMenu;
---
+++ src/menu/StudentMenu.jsx
@@ -0,0 +1,23 @@
+import { MultiLevelMenu, MenuItemCategory } from '@react-admin/ra-navigation';
+import { useNotify } from 'react-admin';
+import React from 'react';
+
+import Receipt from '@material-ui/icons/Receipt';
+import AttachMoney from '@material-ui/icons/AttachMoney';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+import authProvider from '../providers/authProvider';
+
+export function StudentMenu() {
+  const notify = useNotify();
+  const notifyNotImplemented = () => notify('En cours de développement. Ce qui présage quelques exercices pour vous 😉', { type: 'warning' });
+  return (
+    <MultiLevelMenu variant="categories">
+      <MenuItemCategory to="/profile" name="profile" label="Mon profil" icon={<AccountCircle />} />
+      <MenuItemCategory to="/" name="fees" label="Frais" icon={<AttachMoney />} />
+      <MenuItemCategory to="/" name="student-grades" label="Notes" icon={<Receipt />} onClick={notifyNotImplemented} />
+    </MultiLevelMenu>
+  );
+}
+
+export default StudentMenu;
