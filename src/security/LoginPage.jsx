b14a3f3d52bbbc4ff69656f894d29b4359752199
kirioka <hei.amour.2@gmail.com>
2022-08-09 16:07:40 +0300

    fix: add import React

--- src/security/LoginPage.jsx
+++ src/security/LoginPage.jsx
@@ -7,6 +7,7 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { mainTheme } from '../haTheme';
 import CompletePasswordPage from './CompletePasswordPage';
 import authProvider from '../providers/authProvider';
+import React from 'react';
 
 const aCard = (title, subtitle, description1, description2, course) => {
   const syllabus = 'https://drive.google.com/file/d/12Lc4o3jfQOFHIzazPToO2hnGZc8epU3I/view';
