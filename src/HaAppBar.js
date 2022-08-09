1fa0d2e90d49be8aa8b7232b3aa7a013088b9871
kirioka <hei.amour.2@gmail.com>
2022-08-09 20:24:51 +0300

    style: reformat HaAppBar

--- src/HaAppBar.js
+++ src/HaAppBar.js
@@ -1,22 +1,33 @@
-import { AppBar } from '@react-admin/ra-enterprise'
+import React from 'react';
+import { AppBar } from '@react-admin/ra-enterprise';
 
-import { IconButton, Tooltip } from '@material-ui/core'
-import LockIcon from '@material-ui/icons/Lock'
-import authProvider from './providers/authProvider'
+import { IconButton, Tooltip } from '@material-ui/core';
+import LockIcon from '@material-ui/icons/Lock';
+import authProvider from './providers/authProvider.ts';
 
-const UserMenu = () => {
+function UserMenu() {
   const logout = () => {
-    authProvider.logout()
-    window.location.reload()
-  }
+    authProvider.logout();
+    window.location.reload();
+  };
   return (
-    <Tooltip title='Se déconnecter' onClick={logout}>
-      <IconButton color='inherit'>
+    <Tooltip title="Se déconnecter" onClick={logout}>
+      <IconButton color="inherit">
         <LockIcon />
       </IconButton>
     </Tooltip>
-  )
+  );
 }
 
-const HaAppBar = props => <AppBar {...props} languages={[]} color='transparent' elevation={0} userMenu={<UserMenu {...props} />} />
-export default HaAppBar
+function HaAppBar(props) {
+  return (
+    <AppBar
+      {...props}
+      languages={[]}
+      color="transparent"
+      elevation={0}
+      userMenu={<UserMenu {...props} />}
+    />
+  );
+}
+export default HaAppBar;
