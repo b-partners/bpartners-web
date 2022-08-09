c7e95d8ac02dd2624a64e2433317a195ae9c933f
kirioka <hei.amour.2@gmail.com>
2022-08-09 20:27:56 +0300

    style: reformat HaLayout

--- src/HaLayout.js
+++ src/HaLayout.js
@@ -1,15 +1,16 @@
-import { Layout } from '@react-admin/ra-enterprise'
-import { AppLocationContext } from '@react-admin/ra-navigation'
+import React from 'react';
+import { Layout } from '@react-admin/ra-enterprise';
+import { AppLocationContext } from '@react-admin/ra-navigation';
 
-import HaAppBar from './HaAppBar'
-import HaMenu from './menu/HaMenu'
+import HaAppBar from './HaAppBar';
+import HaMenu from './menu/HaMenu';
 
-const HaLayout = props => {
+function HaLayout(props) {
   return (
     <AppLocationContext>
       <Layout {...props} appBar={HaAppBar} menu={HaMenu} />
     </AppLocationContext>
-  )
+  );
 }
 
-export default HaLayout
+export default HaLayout;
