ce1617b358919272859c26a9098cd1e04a5f8fef
kirioka <hei.amour.2@gmail.com>
2022-08-09 20:15:17 +0300

    style: reformat and rename app.js to app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Admin } from '@react-admin/ra-enterprise';
+import { CustomRoutes } from 'react-admin';
+import { Resource } from '@react-admin/ra-rbac';
+
+import { Route } from 'react-router-dom';
+
+import polyglotI18nProvider from 'ra-i18n-polyglot';
+import frenchMessages from 'ra-language-french';
+
+import dataProvider from './providers/dataProvider.ts';
+import authProvider from './providers/authProvider.ts';
+
+import profile from './operations/profile';
+import students from './operations/students';
+import teachers from './operations/teachers';
+
+import fees from './operations/fees';
+import payments from './operations/payments';
+
+import studentGrades from './operations/studentGrades';
+
+import MyLayout from './HaLayout';
+import HaLoginPage from './security/LoginPage';
+
+function App() {
+  return (
+    <Admin
+      title="HEI Admin"
+      authProvider={authProvider}
+      dataProvider={dataProvider}
+      i18nProvider={polyglotI18nProvider(() => frenchMessages, 'fr')}
+      loginPage={HaLoginPage}
+      layout={MyLayout}
+    >
+      <Resource name="profile" />
+      <Resource name="students" {...students} />
+      <Resource name="teachers" {...teachers} />
+
+      <Resource name="fees" {...fees} />
+      <Resource name="payments" {...payments} />
+
+      <Resource name="student-grades" {...studentGrades} />
+
+      <CustomRoutes>
+        <Route exact path="/profile" element={<profile.show />} />
+
+        <Route exact path="/students/:studentId/fees" element={<fees.list />} />
+        <Route exact path="/students/:studentId/fees/create" element={<fees.create />} />
+        <Route exact path="/fees/:feeId/show" element={<fees.show />} />
+        <Route exact path="/fees" element={<fees.listByStatus />} />
+
+        <Route exact path="/fees/:feeId/payments" element={<payments.list />} />
+        <Route exact path="/fees/:feeId/payments/create" element={<payments.create />} />
+      </CustomRoutes>
+    </Admin>
+  );
+}
+
+export default App;
