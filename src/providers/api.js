8f574d302b1af212a138e646d2253f2b2374dce1
kirioka <hei.amour.2@gmail.com>
2022-08-10 14:27:46 +0300

    fix: fix api's modules import

--- src/providers/api.js
+++ src/providers/api.js
@@ -1,5 +1,5 @@
-import { UsersApi, PayingApi } from '../gen/haClient'
-import authProvider from './authProvider'
+import { UsersApi, PayingApi } from '../gen/haClient/api.ts';
+import authProvider from './authProvider.ts';
 
-export const usersApi = () => new UsersApi(authProvider.getCachedAuthConf())
-export const payingApi = () => new PayingApi(authProvider.getCachedAuthConf())
+export const usersApi = () => new UsersApi(authProvider.getCachedAuthConf());
+export const payingApi = () => new PayingApi(authProvider.getCachedAuthConf());
