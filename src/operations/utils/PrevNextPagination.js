cf2f9422300fa7d4f2d8aac977c9f3e248cc88aa
kirioka <hei.amour.2@gmail.com>
2022-08-10 14:39:56 +0300

    style: reformat PrevNexPagination

--- src/operations/utils/PrevNextPagination.js
+++ src/operations/utils/PrevNextPagination.js
@@ -1,59 +1,63 @@
-import { useState } from 'react'
-import { useListContext } from 'react-admin'
+import React, { useState } from 'react';
+import { useListContext } from 'react-admin';
 
-import { Button, Toolbar, Typography } from '@material-ui/core'
-import ChevronLeft from '@material-ui/icons/ChevronLeft'
-import ChevronRight from '@material-ui/icons/ChevronRight'
+import { Button, Toolbar, Typography } from '@material-ui/core';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
 
-const pageSize = 10
+const pageSize = 10;
 
 const haSetPerPage = (setPerPage, setPage, page) => {
-  setPerPage(pageSize)
-  setPage(page) // setPage has to be called after setPerPage, otherwise react-admin fails...
-}
+  setPerPage(pageSize);
+  setPage(page); // setPage has to be called after setPerPage, otherwise react-admin fails...
+};
 
-const PrevNextPagination = () => {
-  var [lastPage, setLastPage] = useState(null)
-  const { page, data, isLoading, setPage, setPerPage } = useListContext()
-  haSetPerPage(setPerPage, setPage, page)
-  const resourcesCount = data ? Object.keys(data).length : 0
+function PrevNextPagination() {
+  const [lastPage, setLastPage] = useState(null);
+  const {
+    page, data, isLoading, setPage, setPerPage,
+  } = useListContext();
+  haSetPerPage(setPerPage, setPage, page);
+  const resourcesCount = data ? Object.keys(data).length : 0;
   if (!lastPage && lastPage !== 0 /* TODO(empty-pages): test! */ && !isLoading && resourcesCount === 0) {
-    lastPage = page - 1
-    setLastPage(lastPage)
-    setPage(lastPage)
+    setLastPage(page - 1);
+    setPage(page - 1);
   }
   if (lastPage && page === 1) {
     // react-admin redirects to page 1 if no more data to show
     // We dont't like that behavior!
     // Let the user stay on the last page.
-    setPage(lastPage)
+    setPage(lastPage);
   }
 
   const onPrevClick = () => {
-    setPage(page - 1)
-    setLastPage(null)
-  }
+    setPage(page - 1);
+    setLastPage(null);
+  };
   return (
     <Toolbar>
       {page > 1 && (
-        <Button color='primary' key='prev' onClick={onPrevClick}>
+        <Button color="primary" key="prev" onClick={onPrevClick}>
           <ChevronLeft />
           Précédent
         </Button>
       )}
       {(!lastPage || page < lastPage) && (
-        <Button color='primary' key='next' onClick={() => setPage(page + 1)}>
+        <Button color="primary" key="next" onClick={() => setPage(page + 1)}>
           Suivant
           <ChevronRight />
         </Button>
       )}
       <div style={{ marginLeft: 'auto' }}>
-        <Typography variant='body2'>
-          Page : {page} &nbsp;&nbsp;Taille : {resourcesCount}
+        <Typography variant="body2">
+          Page :
+          {page}
+          &nbsp;&nbsp;Taille :
+          {resourcesCount}
         </Typography>
       </div>
     </Toolbar>
-  )
+  );
 }
 
-export default PrevNextPagination
+export default PrevNextPagination;
