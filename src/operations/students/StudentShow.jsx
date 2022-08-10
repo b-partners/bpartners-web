8edba9f21dfbc128d3e29051a378ca25f507d9c5
kirioka <hei.amour.2@gmail.com>
2022-08-10 15:06:16 +0300

    refactor: rename StudentShow.js -> StudentShow.jsx

---
+++ src/operations/students/StudentShow.jsx
@@ -0,0 +1,33 @@
+import {
+  Button, Show, EditButton, TopToolbar, Link, useRecordContext,
+} from 'react-admin';
+
+import Money from '@material-ui/icons/AttachMoney';
+import { ProfileLayout } from '../profile/ProfileShow';
+
+import authProvider from '../../providers/authProvider';
+
+function ActionsOnShow({ basePath, data, resource }) {
+  const record = useRecordContext();
+  return (
+    <TopToolbar disableGutters>
+      <EditButton basePath={basePath} resource={resource} record={data} />
+      {record && (
+        <Button label="Frais" aria-label="fees" component={Link} to={`/students/${record.id}/fees`}>
+          <Money />
+        </Button>
+      )}
+    </TopToolbar>
+  );
+}
+
+function StudentShow(props) {
+  const role = authProvider.getCachedRole();
+  return (
+    <Show title="Étudiants" {...props} actions={role === 'MANAGER' ? <ActionsOnShow /> : null}>
+      <ProfileLayout />
+    </Show>
+  );
+}
+
+export default StudentShow;
