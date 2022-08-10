6df793c3df4ae6fa723a675b6ebff221fced8e9e
kirioka <hei.amour.2@gmail.com>
2022-08-10 16:30:20 +0300

    style: refacto ProfileShow.jsx

--- src/operations/profile/ProfileShow.jsx
+++ src/operations/profile/ProfileShow.jsx
@@ -1,40 +1,43 @@
-import { DateField, EmailField, FunctionField, SimpleShowLayout, Show, TextField } from 'react-admin'
+import React from 'react';
+import {
+  DateField, EmailField, FunctionField, SimpleShowLayout, Show, TextField,
+} from 'react-admin';
 
-import { unexpectedValue } from '../utils/typography'
+import { unexpectedValue } from '../utils/typography';
 
-export const ProfileLayout = () => {
-  const sexRenderer = user => {
-    if (user.sex === 'M') return 'Homme'
-    if (user.sex === 'F') return 'Femme'
-    return unexpectedValue
-  }
-  const statusRenderer = user => {
-    if (user.status === 'ENABLED') return 'Actif·ve'
-    if (user.status === 'DISABLED') return 'Suspendu·e'
-    return unexpectedValue
-  }
+export function ProfileLayout() {
+  const sexRenderer = (user) => {
+    if (user.sex === 'M') return 'Homme';
+    if (user.sex === 'F') return 'Femme';
+    return unexpectedValue;
+  };
+  const statusRenderer = (user) => {
+    if (user.status === 'ENABLED') return 'Actif·ve';
+    if (user.status === 'DISABLED') return 'Suspendu·e';
+    return unexpectedValue;
+  };
   return (
     <SimpleShowLayout>
-      <TextField source='ref' label='Référence' />
-      <TextField source='first_name' id='first_name' label='Prénom(s)' />
-      <TextField source='last_name' label='Nom(s)' />
-      <FunctionField label='Sexe' render={sexRenderer} />
-      <TextField label='Téléphone' source='phone' />
-      <DateField source='birth_date' label='Date de naissance' locales='fr-FR' />
-      <TextField source='address' label='Adresse' component='pre' />
-      <EmailField source='email' label='Email' />
-      <DateField source='entrance_datetime' label="Date d'entrée chez HEI" locales='fr-FR' />
-      <FunctionField label='Statut' render={statusRenderer} />
+      <TextField source="ref" label="Référence" />
+      <TextField source="first_name" id="first_name" label="Prénom(s)" />
+      <TextField source="last_name" label="Nom(s)" />
+      <FunctionField label="Sexe" render={sexRenderer} />
+      <TextField label="Téléphone" source="phone" />
+      <DateField source="birth_date" label="Date de naissance" locales="fr-FR" />
+      <TextField source="address" label="Adresse" component="pre" />
+      <EmailField source="email" label="Email" />
+      <DateField source="entrance_datetime" label="Date d'entrée chez HEI" locales="fr-FR" />
+      <FunctionField label="Statut" render={statusRenderer} />
     </SimpleShowLayout>
-  )
+  );
 }
 
-const ProfileShow = () => {
+function ProfileShow() {
   return (
-    <Show id={null} resource='profile' basePath='/profile' title='Mon profil'>
+    <Show id={null} resource="profile" basePath="/profile" title="Mon profil">
       <ProfileLayout />
     </Show>
-  )
+  );
 }
 
-export default ProfileShow
+export default ProfileShow;
