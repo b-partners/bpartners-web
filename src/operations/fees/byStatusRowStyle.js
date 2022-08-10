fab3ce7213d33fdd6c7400259d9a05abd8e7a0ef
kirioka <hei.amour.2@gmail.com>
2022-08-10 16:40:34 +0300

    style: refacto byStatusRowSytle.js

--- src/operations/fees/byStatusRowStyle.js
+++ src/operations/fees/byStatusRowStyle.js
@@ -1,10 +1,11 @@
-import { mainTheme } from '../../haTheme'
+import { mainTheme } from '../../haTheme';
 
-const rowStyle = (record, _index) => {
-  const lateColor = record.status === 'LATE' ? mainTheme.palette.error.light : 'inherit'
+const rowStyle = (record) => {
+  // second params: (, _index)
+  const lateColor = record.status === 'LATE' ? mainTheme.palette.error.light : 'inherit';
   return {
-    backgroundColor: record.status === 'PAID' ? mainTheme.palette.grey[300] : lateColor
-  }
-}
+    backgroundColor: record.status === 'PAID' ? mainTheme.palette.grey[300] : lateColor,
+  };
+};
 
-export default rowStyle
+export default rowStyle;
