51558cf78d3db8c6805aeb81ba0c95365f2c6666
kirioka <hei.amour.2@gmail.com>
2022-08-10 16:36:07 +0300

    style: refacto ProfilEdit.jsx

--- src/operations/profile/ProfileEdit.jsx
+++ src/operations/profile/ProfileEdit.jsx
@@ -1,33 +1,40 @@
-import { SimpleForm, TextInput, DateInput, RadioButtonGroupInput, Edit } from 'react-admin'
-import EditToolbar from '../utils/EditToolBar'
-import SexRadioButton from '../utils/SexRadioButton'
+import React from 'react';
+import {
+  SimpleForm, TextInput, DateInput, RadioButtonGroupInput, Edit,
+} from 'react-admin';
+import EditToolbar from '../utils/EditToolBar';
+import SexRadioButton from '../utils/SexRadioButton';
 
-const StatusRadioButton = () => (
-  <RadioButtonGroupInput
-    source='status'
-    label='Statut'
-    choices={[
-      { id: 'ENABLED', name: 'Actif·ve' },
-      { id: 'DISABLED', name: 'Inactif·ve' }
-    ]}
-  />
-)
+function StatusRadioButton() {
+  return (
+    <RadioButtonGroupInput
+      source="status"
+      label="Statut"
+      choices={[
+        { id: 'ENABLED', name: 'Actif·ve' },
+        { id: 'DISABLED', name: 'Inactif·ve' },
+      ]}
+    />
+  );
+}
 
-const ProfileEdit = props => (
-  <Edit {...props}>
-    <SimpleForm toolbar={<EditToolbar />}>
-      <TextInput source='ref' label='Référence' fullWidth={true} />
-      <TextInput source='first_name' label='Prénom·s' fullWidth={true} />
-      <TextInput source='last_name' label='Nom·s' fullWidth={true} />
-      <TextInput source='email' fullWidth={true} />
-      <TextInput multiline source='address' label='Adresse' fullWidth={true} />
-      <SexRadioButton />
-      <TextInput source='phone' label='Téléphone' fullWidth={true} />
-      <DateInput source='birth_date' label='Date de naissance' fullWidth={true} />
-      <DateInput source='entrance_datetime' label="Date d'entrée chez HEI" fullWidth={true} />
-      <StatusRadioButton />
-    </SimpleForm>
-  </Edit>
-)
+function ProfileEdit(props) {
+  return (
+    <Edit {...props}>
+      <SimpleForm toolbar={<EditToolbar />}>
+        <TextInput source="ref" label="Référence" fullWidth="true" />
+        <TextInput source="first_name" label="Prénom·s" fullWidth="true" />
+        <TextInput source="last_name" label="Nom·s" fullWidth="true" />
+        <TextInput source="email" fullWidth="true" />
+        <TextInput multiline source="address" label="Adresse" fullWidth="true" />
+        <SexRadioButton />
+        <TextInput source="phone" label="Téléphone" fullWidth="true" />
+        <DateInput source="birth_date" label="Date de naissance" fullWidth="true" />
+        <DateInput source="entrance_datetime" label="Date d'entrée chez HEI" fullWidth="true" />
+        <StatusRadioButton />
+      </SimpleForm>
+    </Edit>
+  );
+}
 
-export default ProfileEdit
+export default ProfileEdit;
