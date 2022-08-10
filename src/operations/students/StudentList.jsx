101fdac1351e90e1fa3d048916262a7858c7a865
kirioka <hei.amour.2@gmail.com>
2022-08-10 15:06:58 +0300

    refactor: rename StudentList.js -> StudentList.jsx

---
+++ src/operations/students/StudentList.jsx
@@ -0,0 +1,25 @@
+import { List } from '@react-admin/ra-rbac';
+import {
+  TextField, Datagrid, ShowButton, EditButton,
+} from 'react-admin';
+
+import authProvider from '../../providers/authProvider';
+
+import { profileFilters } from '../profile';
+import PrevNextPagination from '../utils/PrevNextPagination';
+
+function StudentList(props) {
+  const role = authProvider.getCachedRole();
+  return (
+    <List {...props} label="Étudiants" bulkActionButtons={false} filters={profileFilters} pagination={<PrevNextPagination />}>
+      <Datagrid rowClick="show">
+        <TextField source="ref" label="Référence" />
+        <TextField source="first_name" label="Prénom·s" />
+        <TextField source="last_name" label="Nom·s" />
+        {role === 'MANAGER' ? <EditButton /> : <ShowButton />}
+      </Datagrid>
+    </List>
+  );
+}
+
+export default StudentList;
