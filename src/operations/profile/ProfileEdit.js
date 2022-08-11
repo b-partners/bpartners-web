ee9502090d673681355fdca811653e6cad203bf8
kirioka <hei.amour.2@gmail.com>
2022-08-11 15:05:42 +0300

    refacto: change user's information on profil

--- src/operations/profile/ProfileEdit.js
+++ src/operations/profile/ProfileEdit.js
@@ -1,6 +1,5 @@
-import { SimpleForm, TextInput, DateInput, RadioButtonGroupInput, Edit } from 'react-admin'
+import { SimpleForm, TextInput, RadioButtonGroupInput, Edit } from 'react-admin'
 import EditToolbar from '../utils/EditToolBar'
-import SexRadioButton from '../utils/SexRadioButton'
 
 const StatusRadioButton = () => (
   <RadioButtonGroupInput
@@ -16,16 +15,9 @@ const StatusRadioButton = () => (
 const ProfileEdit = props => (
   <Edit {...props}>
     <SimpleForm toolbar={<EditToolbar />}>
-      <TextInput source='ref' label='Référence' fullWidth={true} />
       <TextInput source='first_name' label='Prénom·s' fullWidth={true} />
       <TextInput source='last_name' label='Nom·s' fullWidth={true} />
       <TextInput source='email' fullWidth={true} />
-      <TextInput multiline source='address' label='Adresse' fullWidth={true} />
-      <SexRadioButton />
-      <TextInput source='phone' label='Téléphone' fullWidth={true} />
-      <DateInput source='birth_date' label='Date de naissance' fullWidth={true} />
-      <DateInput source='entrance_datetime' label="Date d'entrée chez HEI" fullWidth={true} />
-      <StatusRadioButton />
     </SimpleForm>
   </Edit>
 )
