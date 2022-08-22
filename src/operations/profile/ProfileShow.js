a971fc0e1c579154ef331e256f6ba899d5a11faa
kirioka <hei.amour.2@gmail.com>
2022-08-22 13:51:01 +0300

    feat: profil viwer with mock

--- src/operations/profile/ProfileShow.js
+++ src/operations/profile/ProfileShow.js
@@ -6,17 +6,17 @@ export const ProfileLayout = () => {
       <TextField source='first_name' id='first_name' label='Prénom(s)' />
       <TextField source='last_name' id='first_name' label='Nom(s)' />
       <EmailField source='email' label='Email' />
-      <TextField source='phone_number' id='first_name' label='Téléphone' />
+      <TextField source='mobilePhoneNumber' id='first_name' label='Téléphone' />
     </SimpleShowLayout>
   );
 };
 
 const ProfileShow = ({ id }) => {
-  if (!id) {
-    return null;
-  }
+  // if (!id) {
+  //   return null;
+  // }
   return (
-    <Show id={null} resource='profile' basePath='/profile' title='Mon profil'>
+    <Show id='uuid' resource='profile' basePath='/profile' title='Mon profil'>
       <ProfileLayout />
     </Show>
   );
--- src/providers/profile-provider.ts
+++ src/providers/profile-provider.ts
@@ -1,21 +1,30 @@
-import { usersApi } from './api';
-import { HaDataProviderType } from './ha-data-provider-type';
+import {usersApi} from './api';
+import {HaDataProviderType} from './ha-data-provider-type';
+import axios from 'axios';
+import {getUrlInformation} from "../operations/utils/getUrlInforamtion";
 
 const profileProvider: HaDataProviderType = {
-  async getOne(id: string) {
-    const role = 'MANAGER';
-    if (role === 'MANAGER') {
-      return usersApi()
-        .getManagerById(id)
-        .then(result => result.data);
-    }
-  },
-  getList: function (page: number, perPage: number, filter: any): Promise<any[]> {
-    throw new Error('Function not implemented.');
-  },
-  saveOrUpdate: function (resources: any[]): Promise<any[]> {
-    throw new Error('Function not implemented.');
-  },
+    async getOne(id: string) {
+        return axios.post("http://localhost:8080/token", {code: getUrlInformation().code})
+            .then((res1) => axios.get("http://localhost:8080/whoami", {
+                headers: {
+                    'Authorization': 'Bearer ' + res1.data.idToken
+                }
+            })
+                .then((res2) => res2.data.user));
+        // const role = 'MANAGER';
+        // if (role === 'MANAGER') {
+        //   return usersApi()
+        //     .getManagerById(id)
+        //     .then(result => result.data);
+        // }
+    },
+    getList: function (page: number, perPage: number, filter: any): Promise<any[]> {
+        throw new Error('Function not implemented.');
+    },
+    saveOrUpdate: function (resources: any[]): Promise<any[]> {
+        throw new Error('Function not implemented.');
+    },
 };
 
 export default profileProvider;
