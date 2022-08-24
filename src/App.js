a48e149b947b79aa3df64f7cdfc1749625709f2e
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-08-24 14:06:22 +0300

    chore: conditionnaly setting data provider

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
-import { Admin } from '@react-admin/ra-enterprise';
-import { CustomRoutes } from 'react-admin';
-import { Resource } from '@react-admin/ra-rbac';
+import { Admin } from '@react-admin/ra-enterprise'
+import { CustomRoutes } from 'react-admin'
+import { Resource } from '@react-admin/ra-rbac'
 
-import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom'
 
-import dataProvider from './providers/data-provider';
-import authProvider from './providers/auth-provider.ts';
+import dataProvider from './providers/data-provider'
+import authProvider from './providers/auth-provider.ts'
 
-import polyglotI18nProvider from 'ra-i18n-polyglot';
-import frenchMessages from 'ra-language-french';
+import polyglotI18nProvider from 'ra-i18n-polyglot'
+import frenchMessages from 'ra-language-french'
 
-import { mockDataProvider } from './providers/mock-provider';
-import student from './operations/students';
-import profile from './operations/profile';
-import transaction from './operations/transactions';
+import { mockDataProvider } from './providers/mock-provider'
+import profile from './operations/profile'
+import transaction from './operations/transactions'
 
-import MyLayout from './HaLayout';
-import HaLoginPage from './security/LoginPage';
+import MyLayout from './HaLayout'
+import HaLoginPage from './security/LoginPage'
 
 const App = () => (
   <Admin
     title='HEI Admin'
     authProvider={authProvider}
-    dataProvider={mockDataProvider}
+    dataProvider={process.env.REACT_APP_ENV === 'mock' ? mockDataProvider : dataProvider}
     i18nProvider={polyglotI18nProvider(() => frenchMessages, 'fr')}
     loginPage={HaLoginPage}
     layout={MyLayout}
   >
     <Resource name='profile' />
-    <Resource name='transactions' list={transaction.list} show={student.show} icon={transaction.icon} />
+    <Resource name='transactions' list={transaction.list} show={transaction.show} icon={transaction.icon} />
 
     <CustomRoutes>
       <Route exact path='/profile' element={<profile.show />} />
       <Route exact path='/transactions' element={<transaction.list />} />
     </CustomRoutes>
   </Admin>
-);
+)
 
-export default App;
+export default App
