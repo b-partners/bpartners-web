090914b59415821680890de3966e2b357036d630
kirioka <hei.amour.2@gmail.com>
2022-08-25 15:08:01 +0300

    feat: add url params getter and show the result

--- src/operations/error/ErrorShow.js
+++ src/operations/error/ErrorShow.js
@@ -1,4 +1,7 @@
 import { Container, makeStyles, Typography } from '@material-ui/core';
+import { red } from '@material-ui/core/colors';
+import {useLocation} from "react-router-dom"; 
+import getParams from "../utils/getParams";
 import errorImg from '../../assets/img/errorImg.png';
 
 const useStyles = makeStyles(() => ({
@@ -11,16 +14,22 @@ const useStyles = makeStyles(() => ({
   image: {
     maxWidth: '30%',
   },
+  typography: {
+    color: red["A200"],
+    marginTop: "1.5rem"
+  }
 }));
 
 const ErrorShow = () => {
   const classes = useStyles();
-
+  const location = useLocation()
+  const message = getParams(location.search, "message");
+  
   return (
     <Container className={classes.container}>
       <Container>
         <img src={errorImg} alt='' className={classes.image} />
-        <Typography variant='h6' className={classes.typography}>Message d'erreur</Typography>
+        <Typography variant='h6' className={classes.typography} paragraphe>{message}</Typography>
       </Container>
     </Container>
   );
