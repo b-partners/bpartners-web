c89e06cd6286e9810ee18f5b5367a3982eeb3141
RAMANANTSIRESY Amour Bien Aimé <95756837+AmourRamanantsiresy@users.noreply.github.com>
2022-08-30 16:58:04 +0300

    fix: show user by id error (#12)
    * fix: show user by id error
    
    * fix: fix available information

--- src/providers/profile-provider.ts
+++ src/providers/profile-provider.ts
@@ -1,36 +1,27 @@
-import { HaDataProviderType } from './ha-data-provider-type'
-import axios from 'axios'
-import getParams from 'src/operations/utils/getParams'
+import { HaDataProviderType } from './ha-data-provider-type';
+import httpClient from 'src/config/http-client';
 
 const profileProvider: HaDataProviderType = {
   async getOne(id: string) {
-    const { search } = document.location
-    const code = getParams(search, 'code')
-    const apiUrl: string = process.env.REACT_APP_API_URL || ''
     try {
-      const params = {
-        code: getParams(search, 'code') || '',
-        successUrl: 'https://dashboard-dev.bpartners.app',
-        failureUrl: 'https://dashboard-dev.bpartners.app/error'
-      }
-      const bearerToken = await axios.post(`${apiUrl}/token`, params)
-
-      const userInformation = await axios.get(`${apiUrl}/whoami`, {
+      const { data } = await httpClient.get('whoami', {
         headers: {
-          'Authorization': `Bearer ${bearerToken.data.refreshToken}`
+          'Authorization': `Bearer ${localStorage.getItem("accessToken") || ''}`
         }
-      })
-      return userInformation.data.user
+      });
+      console.log(data)
+      localStorage.setItem("userId", data.user.id || '');
+      return data.user;
     } catch {
-      throw new Error('Bad url')
+      throw new Error('Bad url');
     }
   },
-  getList: function(page: number, perPage: number, filter: any): Promise<any[]> {
-    throw new Error('Function not implemented.')
+  getList: function (page: number, perPage: number, filter: any): Promise<any[]> {
+    throw new Error('Function not implemented.');
   },
-  saveOrUpdate: function(resources: any[]): Promise<any[]> {
-    throw new Error('Function not implemented.')
+  saveOrUpdate: function (resources: any[]): Promise<any[]> {
+    throw new Error('Function not implemented.');
   }
-}
+};
 
-export default profileProvider
+export default profileProvider;
