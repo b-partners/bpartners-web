ecfb93b8e2a7475ab1174414d95ab264beb2bb94
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-08-30 18:06:10 +0300

    fix: enhancing get profile by id

--- src/providers/profile-provider.ts
+++ src/providers/profile-provider.ts
@@ -1,27 +1,30 @@
-import { HaDataProviderType } from './ha-data-provider-type';
-import httpClient from 'src/config/http-client';
+import { HaDataProviderType } from './ha-data-provider-type'
+import httpClient from 'src/config/http-client'
 
 const profileProvider: HaDataProviderType = {
   async getOne(id: string) {
+    const accessToken = localStorage.getItem('accessToken')
+    if (!accessToken) {
+      return
+    }
     try {
       const { data } = await httpClient.get('whoami', {
         headers: {
-          'Authorization': `Bearer ${localStorage.getItem("accessToken") || ''}`
+          'Authorization': `Bearer ${accessToken}`
         }
-      });
-      console.log(data)
-      localStorage.setItem("userId", data.user.id || '');
-      return data.user;
+      })
+      localStorage.setItem('userId', data.user.id || '')
+      return data.user
     } catch {
-      throw new Error('Bad url');
+      throw new Error('Bad url')
     }
   },
-  getList: function (page: number, perPage: number, filter: any): Promise<any[]> {
-    throw new Error('Function not implemented.');
+  getList: function(page: number, perPage: number, filter: any): Promise<any[]> {
+    throw new Error('Function not implemented.')
   },
-  saveOrUpdate: function (resources: any[]): Promise<any[]> {
-    throw new Error('Function not implemented.');
+  saveOrUpdate: function(resources: any[]): Promise<any[]> {
+    throw new Error('Function not implemented.')
   }
-};
+}
 
-export default profileProvider;
+export default profileProvider
