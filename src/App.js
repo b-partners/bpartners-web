74e2bee332a56cc0fdf5fa8939d3abbde6e3bf49
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-08-31 15:24:49 +0300

    chore: using browser history routing

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { Admin } from '@react-admin/ra-enterprise'
 import { CustomRoutes } from 'react-admin'
 import { Resource } from '@react-admin/ra-rbac'
 
-import { Route } from 'react-router-dom'
-import authProvider from './providers/auth-provider.ts';
-import dataProvider from './providers/data-provider';
+import { Route, BrowserRouter } from 'react-router-dom'
+import authProvider from './providers/auth-provider.ts'
+import dataProvider from './providers/data-provider'
 import polyglotI18nProvider from 'ra-i18n-polyglot'
 import frenchMessages from 'ra-language-french'
 
@@ -16,22 +16,24 @@ import MyLayout from './HaLayout'
 import HaLoginPage from './security/LoginPage'
 
 const App = () => (
-  <Admin
-    title='BPartners Dashboard'
-    authProvider={authProvider}
-    dataProvider={process.env.REACT_APP_ENV === 'mock' ? mockDataProvider : dataProvider}
-    i18nProvider={polyglotI18nProvider(() => frenchMessages, 'fr')}
-    loginPage={HaLoginPage}
-    layout={MyLayout}
-  >
-    <Resource name='profile' />
-    <Resource name='transactions' list={transaction.list} show={transaction.show} icon={transaction.icon} />
+  <BrowserRouter>
+    <Admin
+      title='BPartners Dashboard'
+      authProvider={authProvider}
+      dataProvider={process.env.REACT_APP_ENV === 'mock' ? mockDataProvider : dataProvider}
+      i18nProvider={polyglotI18nProvider(() => frenchMessages, 'fr')}
+      loginPage={HaLoginPage}
+      layout={MyLayout}
+    >
+      <Resource name='profile' />
+      <Resource name='transactions' list={transaction.list} show={transaction.show} icon={transaction.icon} />
 
-    <CustomRoutes>
-      <Route exact path='/profile' element={<profile.show />} />
-      <Route exact path='/transactions' element={<transaction.list />} />
-    </CustomRoutes>
-  </Admin>
+      <CustomRoutes>
+        <Route exact path='/profile' element={<profile.show />} />
+        <Route exact path='/transactions' element={<transaction.list />} />
+      </CustomRoutes>
+    </Admin>
+  </BrowserRouter>
 )
 
 export default App
