42e2fe9a3ccccd1a7c23f781e33fb416e6c36cac
Lou Maurica <lou.maurica@hyland.com>
2022-09-02 13:30:14 +0200

    fix: authentication from phone

--- src/operations/transactions/index.js
+++ src/operations/transactions/index.js
@@ -2,11 +2,11 @@ import WorkIcon from '@material-ui/icons/Work';
 import TransactionShow from './TransactionShow';
 import TransactionList from './TransactionList';
 
-const Transactions = {
+const transactions = {
   list: TransactionList,
   show: TransactionShow,
   icon: WorkIcon,
   options: { label: 'Transactions' },
 };
 
-export default Transactions;
+export default transactions;
--- src/operations/utils/getParams.ts
+++
@@ -1,3 +0,0 @@
-const getParams = (search: string, key: string) => new URLSearchParams(search).get(key);
-
-export default getParams;
