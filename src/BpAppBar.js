5f71a166d19606482fc766fd340978d59f960494
Yume <96466759+YumeT023@users.noreply.github.com>
2022-09-05 17:54:36 +0300

    feat: show firstName on the top right

--- src/BpAppBar.js
+++ src/BpAppBar.js
@@ -1,8 +1,13 @@
 import { Box } from '@mui/material';
 
 import { IconButton, Tooltip } from '@material-ui/core';
+import { TextField } from '@react-admin/ra-enterprise';
 import LockIcon from '@material-ui/icons/Lock';
 import authProvider from './providers/auth-provider';
+import accountProvider from './providers/account-provider';
+import profileProvider from './providers/profile-provider';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useAuthState } from 'react-admin';
 
 const LogoutButton = () => {
   const logout = () => {
@@ -18,12 +23,27 @@ const LogoutButton = () => {
   );
 };
 
-const BpAppBar = props => (
-  <Box {...props} display='flex' justifyContent='flex-end' mt={-5}>
-    <Box>
-      Bonjour <b>Fonenantsoa{/*TODO*/}</b> !
-      <LogoutButton />
+
+const BpAppBar = props => {
+  const userId = authProvider.getCachedWhoami().id;
+  const [name, setName] = useState("");
+
+  // memoize the function to avoid creating this function all the time
+  const getFirstName = useCallback(() => {
+    accountProvider
+      .getOne(userId)
+      .then((data) => setName(data.user.firstName));
+  }, []);
+
+  useEffect(() => getFirstName(), []);
+
+  return (
+    <Box {...props} display='flex' justifyContent='flex-end' mt={-5}>
+      <Box>
+        Bonjour <b>{name}</b> !
+        <LogoutButton />
+      </Box>
     </Box>
-  </Box>
-);
+  )
+};
 export default BpAppBar;
