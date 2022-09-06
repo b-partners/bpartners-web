abbb1fc9f0e189aef1ad9d7ca949711b6f73428e
Yume <96466759+YumeT023@users.noreply.github.com>
2022-09-06 17:26:22 +0300

    test: manual click instead of window.location

--- src/__tests__/Account.cy.js
+++ src/__tests__/Account.cy.js
@@ -4,7 +4,7 @@ import specTitle from 'cypress-sonarqube-reporter/specTitle';
 import App from '../App';
 
 import authProvider from '../providers/auth-provider';
-import { whoami1, user1, token1 } from './mocks/responses/security-api';
+import { whoami1, token1 } from './mocks/responses/security-api';
 
 describe(specTitle('Account'), () => {
   beforeEach(() => {
@@ -14,9 +14,13 @@ describe(specTitle('Account'), () => {
     cy.then(async () => await authProvider.login('dummy', 'dummy', { redirectionStatusUrls: { successurl: 'dummy', FailureUrl: 'dummy' } }));
   });
 
-  it.skip(/*TODO: route is broken*/ 'is displayed on login', () => {
+  it(/*TODO: route is broken*/ 'is displayed on login', () => {
     mount(<App />);
     cy.wait('@whoami'); //TODO: ask backend to fix GET /users/id
+
+    // note(test-redirect)
+    // i do prefer this one instead of processing window.location tedious configuration
+    cy.get('[href="/account"]').click();
 
     cy.contains('Ma société');
     cy.contains('Mon identité');
