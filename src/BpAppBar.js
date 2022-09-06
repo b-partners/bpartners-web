9dba9f36c790939ef1138d6b340ffe8c4da674a7
Yume <96466759+YumeT023@users.noreply.github.com>
2022-09-06 17:21:57 +0300

    style: remove unnecessary imports

--- src/BpAppBar.js
+++ src/BpAppBar.js
@@ -1,13 +1,8 @@
-import { Box } from '@mui/material';
-
-import { IconButton, Tooltip } from '@material-ui/core';
-import { TextField } from '@react-admin/ra-enterprise';
 import LockIcon from '@material-ui/icons/Lock';
 import authProvider from './providers/auth-provider';
 import accountProvider from './providers/account-provider';
-import profileProvider from './providers/profile-provider';
-import { useCallback, useEffect, useMemo, useState } from 'react';
-import { useAuthState } from 'react-admin';
+import { IconButton, Tooltip, Box } from '@material-ui/core';
+import { useCallback, useEffect, useState } from 'react';
 
 const LogoutButton = () => {
   const logout = () => {
@@ -27,12 +22,12 @@ const BpAppBar = props => {
   const userId = authProvider.getCachedWhoami().id;
   const [name, setName] = useState('');
 
-  // memoize the function to avoid creating this function all the time
+  // memoize the function to avoid creating it all the time
   const getFirstName = useCallback(() => {
     accountProvider.getOne(userId).then(data => setName(data.user.firstName));
-  }, []);
+  }, [userId]);
 
-  useEffect(() => getFirstName(), []);
+  useEffect(() => getFirstName(), [getFirstName]);
 
   return (
     <Box {...props} display='flex' justifyContent='flex-end' mt={-5}>
