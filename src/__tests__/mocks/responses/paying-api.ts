b3c70f3d95cb5107b92cea1ea8290ef93b7d6ebd
Lou Maurica <lou.maurica@hyland.com>
2022-09-07 21:30:14 +0200

    test: prettier cypress screens

--- src/__tests__/mocks/responses/paying-api.ts
+++ src/__tests__/mocks/responses/paying-api.ts
@@ -4,7 +4,7 @@ export const transactions1: Array<Transaction> = [
   {
     id: 'transaction1',
     label: "Abonnement BPartners - L'essentiel",
-    reference: 'BP22002',
+    reference: 'BP22001',
     amount: -5,
     category: { label: 'TVA 20%' },
     paymentDatetime: new Date('2022-08-18T05:34:20'),
@@ -12,8 +12,31 @@ export const transactions1: Array<Transaction> = [
   {
     id: 'transaction2',
     label: 'Carrelage 50m2',
-    reference: 'BP22001',
+    reference: 'BP22002',
     amount: 500,
     paymentDatetime: new Date('2022-08-17T03:24:00'),
+  },
+  {
+    id: 'transaction3',
+    label: "Abonnement BPartners - L'essentiel",
+    reference: 'BP22003',
+    amount: -5,
+    category: { label: 'TVA 20%' },
+    paymentDatetime: new Date('2022-07-18T05:34:20'),
+  },
+  {
+    id: 'transaction4',
+    label: 'Carrelage 50m2',
+    reference: 'BP22004',
+    amount: 500,
+    paymentDatetime: new Date('2022-07-17T03:24:00'),
+  },
+  {
+    id: 'transaction5',
+    label: "Abonnement BPartners - L'essentiel",
+    reference: 'BP22005',
+    amount: -5,
+    category: { label: 'TVA 20%' },
+    paymentDatetime: new Date('2022-06-18T05:34:20'),
   },
 ];
