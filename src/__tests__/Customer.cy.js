10ecdf22bd4d01de07643ca98aee765c6060699f
Yume <96466759+YumeT023@users.noreply.github.com>
2022-09-20 15:53:28 +0300

    test: customer

---
+++ src/__tests__/Customer.cy.js
@@ -0,0 +1,38 @@
+import { mount } from '@cypress/react';
+import specTitle from 'cypress-sonarqube-reporter/specTitle';
+import App from 'src/App';
+import authProvider from 'src/providers/auth-provider';
+import { accountHolders1, accounts1 } from './mocks/responses/account-api';
+import { customers1 } from './mocks/responses/customer-api';
+import { token1, user1, whoami1 } from './mocks/responses/security-api';
+
+describe(specTitle('Customers'), () => {
+  beforeEach(() => {
+    //note(login-user1)
+    cy.intercept('POST', '/token', token1);
+    cy.intercept('GET', '/whoami', whoami1).as('whoami');
+    cy.then(async () => await authProvider.login('dummy', 'dummy', { redirectionStatusUrls: { successurl: 'dummy', FailureUrl: 'dummy' } }));
+
+    cy.intercept('GET', '/accounts/mock-account-id1/customers', customers1).as('getCustomers');
+    cy.intercept('GET', `/users/${whoami1.user.id}/accounts`, accounts1).as('getAccount1');
+    cy.intercept('GET', `/users/${whoami1.user.id}/accounts/${accounts1[0].id}/accountHolders`, accountHolders1).as('getAccountHolder1');
+    cy.intercept('GET', `/users/${whoami1.user.id}`, user1).as('getUser1');
+  });
+
+  it('are displayed', () => {
+    mount(<App />);
+    cy.wait('@getUser1');
+    cy.get(':nth-child(2) > .MuiListItem-root').click();
+    cy.wait('@getCustomers');
+
+    cy.contains('2589 Nelm Street');
+    cy.contains('Name 1');
+    cy.contains('Email 3');
+    cy.contains('22 22 22');
+  });
+
+  it('is created with valid input', () => {
+    mount(<App />);
+
+  })
+});
---
+++ src/__tests__/mocks/responses/customer-api.ts
@@ -0,0 +1,25 @@
+import { Customer } from 'src/gen/bpClient';
+
+export const customers1: Array<Customer> = [
+  {
+    id: 'customer1',
+    name: 'Name 1',
+    email: 'Email 1',
+    phone: '11 11 11',
+    address: '2589 Nelm Street'
+  },
+  {
+    id: 'customer2',
+    name: 'Name 2',
+    email: 'Email 2',
+    phone: '22 22 22',
+    address: '4836 Pheasant Ridge Road'
+  },
+  {
+    id: 'customer3',
+    name: 'Name 3',
+    email: 'Email 3',
+    phone: '33 33 33',
+    address: '1321 Pyramid Valley Road'
+  },
+];
