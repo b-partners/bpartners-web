e335e9d7a97176ba57ebadda2c3b2d5b1575eadd
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-10-05 16:48:33 +0300

    fix: renaming exported module

--- src/providers/file-provider.ts
+++ src/providers/file-provider.ts
@@ -1,25 +1,25 @@
-import { v4 as uuid } from 'uuid';
-import { singleAccountGetter } from './account-provider';
-import { FileApi } from './api';
-import authProvider from './auth-provider';
-import { BpDataProviderType } from './bp-data-provider-type';
+import { v4 as uuid } from 'uuid'
+import { singleAccountGetter } from './account-provider'
+import { FileApi } from './api'
+import authProvider from './auth-provider'
+import { BpDataProviderType } from './bp-data-provider-type'
 
-export const filesProvider: BpDataProviderType = {
+export const fileProvider: BpDataProviderType = {
   async getOne(id: string) {
-    const userId = authProvider.getCachedWhoami().user.id;
-    const accountId = (await singleAccountGetter(userId)).id;
+    const userId = authProvider.getCachedWhoami().user.id
+    const accountId = (await singleAccountGetter(userId)).id
     return FileApi()
       .getFileById(accountId, id)
-      .then(({ data }) => data);
+      .then(({ data }) => data)
   },
-  getList: function (page: number, perPage: number, filter: any): Promise<any[]> {
-    throw new Error('Function not implemented.');
+  getList: function(page: number, perPage: number, filter: any): Promise<any[]> {
+    throw new Error('Function not implemented.')
   },
   async saveOrUpdate(resources: any[]): Promise<any[]> {
-    const userId = authProvider.getCachedWhoami().user.id;
-    const accountId = (await singleAccountGetter(userId)).id;
+    const userId = authProvider.getCachedWhoami().user.id
+    const accountId = (await singleAccountGetter(userId)).id
     return FileApi()
       .uploadFile(resources, accountId, uuid())
-      .then(data => [data]);
-  },
-};
+      .then(data => [data])
+  }
+}
