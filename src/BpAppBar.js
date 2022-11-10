c76810c2a8970d72b35a65663883b8146eb23d56
Yume <96466759+YumeT023@users.noreply.github.com>
2022-11-10 11:01:07 +0300

    Feat/145 (#67)
    * feat: beta test warning component
    
    * feat: make warning visible
    
    * test: fix selector
    
    * fix: beta test warning is hidden

--- src/BpAppBar.js
+++ src/BpAppBar.js
@@ -1,17 +1,19 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, useMediaQuery } from '@material-ui/core';
 import { Box } from '@mui/system';
 import { useCallback, useEffect, useState } from 'react';
+import { SidebarToggleButton } from 'react-admin';
 import bpLogo from './assets/bp-logo-full.png';
 import accountProvider from './providers/account-provider';
 import authProvider from './providers/auth-provider';
+import { LongWarning, ShortWarning } from './utils/beta-test-warning';
 
 const useStyle = makeStyles(() => ({
   LOGO: {
-    height: '2.5rem',
+    height: '2.7rem',
   },
   TOOLBAR: {
     zIndex: '999',
-    height: '3rem',
+    height: '3.5rem',
     position: 'fixed',
     left: 0,
     top: 0,
@@ -23,11 +25,16 @@ const useStyle = makeStyles(() => ({
     justifyContent: 'space-between',
     alignItems: 'center',
   },
+  sidebarToggleButton: {
+    height: '2.5rem',
+    width: '2.5rem',
+  },
 }));
 
 const BpAppBar = props => {
   const classes = useStyle();
   const userId = authProvider.getCachedWhoami()?.user?.id;
+  const displayAppBarWarning = useMediaQuery('(min-width: 468px)');
   const [name, setName] = useState('');
 
   const getFirstName = useCallback(() => {
@@ -37,13 +44,22 @@ const BpAppBar = props => {
   useEffect(() => getFirstName(), [getFirstName]);
 
   return (
-    <Box {...props} className={classes.TOOLBAR} sx={{ boxShadow: 1 }}>
-      <img src={bpLogo} alt='bp logo' className={classes.LOGO} />
+    <>
+      <Box {...props} className={classes.TOOLBAR} sx={{ boxShadow: 1 }}>
+        <img src={bpLogo} alt='bp logo' className={classes.LOGO} />
 
-      <Box sx={{ paddingInline: '1rem' }}>
-        Bonjour <b>{name}</b> !
+        <Box sx={{ paddingInline: '1rem' }}>
+          Bonjour <b>{name}</b> !
+        </Box>
+
+        <Box sx={{ display: 'inherit', alignItems: 'center', paddingInline: '.6rem' }}>
+          {displayAppBarWarning && <ShortWarning />}
+          <SidebarToggleButton className={classes.sidebarToggleButton} />
+        </Box>
       </Box>
-    </Box>
+
+      <LongWarning />
+    </>
   );
 };
 export default BpAppBar;
---
+++ src/utils/beta-test-warning.js
@@ -0,0 +1,42 @@
+import { Alert, AlertTitle } from '@mui/material';
+
+const COMMON_STYLE = {
+  border: 'none',
+  opacity: 0.85,
+};
+
+export const LongWarning = () => (
+  <Alert
+    severity='warning'
+    sx={{
+      ...COMMON_STYLE,
+      position: 'fixed',
+      zIndex: 3,
+      bottom: '1rem',
+      right: '1rem',
+      height: 'auto',
+      minWidth: '20rem',
+    }}
+  >
+    <AlertTitle>Avertissement</AlertTitle>
+    <strong>BPartners</strong> est actuellement en beta test
+  </Alert>
+);
+
+export const ShortWarning = () => (
+  <Alert
+    sx={{
+      ...COMMON_STYLE,
+      padding: '.2rem .9rem',
+      opacity: 0.8,
+      mr: 2,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      border: 'none',
+    }}
+    severity='warning'
+  >
+    beta test
+  </Alert>
+);
