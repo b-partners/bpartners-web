724d184ee1fb08f270657e819f6dbca32a2ba23e
Rojotiana Nomena <hei.nomena@gmail.com>
2022-11-18 17:10:50 +0300

    feat: redirection to swan (#79)

--- src/menu/BpMenu.js
+++ src/menu/BpMenu.js
@@ -1,4 +1,4 @@
-import { AccountCircle, Category, Euro, Lock, People, Receipt, Settings, Store } from '@material-ui/icons';
+import { AccountCircle, AccountBalance, Category, Euro, Lock, People, Receipt, Settings, Store } from '@material-ui/icons';
 import { Box } from '@mui/material';
 import { Menu } from 'react-admin';
 import authProvider from '../providers/auth-provider';
@@ -11,24 +11,32 @@ const LogoutButton = () => {
   return <Menu.Item to='#' onClick={logout} name='deconnection' primaryText='Se déconnecter' leftIcon={<Lock />} />;
 };
 
-const BpMenu = () => (
-  <Box sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column', height: '100%' }}>
-    <Menu>
-      <Menu.Item to='/transactions' name='transactions' primaryText='Mes transactions' leftIcon={<Euro />} />
-      <Menu.Item to='/invoice' name='invoice' primaryText='Devis / Facturation' leftIcon={<Receipt />} />
-      <Menu.Item to='/customers' name='customers' primaryText='Mes clients' leftIcon={<People />} />
-      <Menu.Item to='/account' name='account' primaryText='Mon compte' leftIcon={<AccountCircle />} />
-      <Menu.Item to='/products' name='products' primaryText='Mes Produits' leftIcon={<Category />} />
-      <Menu.Item to='/marketplaces' name='marketplaces' primaryText='Les marchés' leftIcon={<Store />} />
-    </Menu>
+const BpMenu = () => {
+  const redirectToSwan = () => {
+    const swanUrl = process.env.REACT_APP_SWAN_URL;
+    window.open(swanUrl, '_blank');
+  };
 
-    <Box sx={{ display: 'flex', alignItems: 'end' }}>
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column', height: '100%' }}>
       <Menu>
-        <Menu.Item to='/configurations' name='configurations' primaryText='Configuration' leftIcon={<Settings />} />
-        <LogoutButton />
+        <Menu.Item to='/transactions' name='transactions' primaryText='Mes transactions' leftIcon={<Euro />} />
+        <Menu.Item to='/invoice' name='invoice' primaryText='Devis / Facturation' leftIcon={<Receipt />} />
+        <Menu.Item to='/customers' name='customers' primaryText='Mes clients' leftIcon={<People />} />
+        <Menu.Item to='/account' name='account' primaryText='Mon compte' leftIcon={<AccountCircle />} />
+        <Menu.Item to='/products' name='products' primaryText='Mes Produits' leftIcon={<Category />} />
+        <Menu.Item to='/marketplaces' name='marketplaces' primaryText='Les marchés' leftIcon={<Store />} />
       </Menu>
+
+      <Box sx={{ display: 'flex', alignItems: 'end' }}>
+        <Menu>
+          <Menu.Item to='/' onClick={redirectToSwan} primaryText='Aller sur Swan' name='swan' leftIcon={<AccountBalance />} />
+          <Menu.Item to='/configurations' name='configurations' primaryText='Configuration' leftIcon={<Settings />} />
+          <LogoutButton />
+        </Menu>
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default BpMenu;
