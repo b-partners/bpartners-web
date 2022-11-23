ac8d4daf1510c3d58bd0d830db980bf3259d81e3
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-11-23 14:46:56 +0300

    feat: implementing onboarding

--- src/bpTheme.js
+++ src/bpTheme.js
@@ -1,23 +1,24 @@
-import { defaultTheme } from 'react-admin';
+import { defaultTheme } from 'react-admin'
 
 export const BP_COLOR = {
   10: '#7A003D',
   20: '#660033',
   30: '#582d37',
   40: '#F1E4E7',
-  solid_grey: 'rgb(0, 0, 0, 0.05)',
-};
+  solid_grey: 'rgb(0, 0, 0, 0.05)'
+}
 
 const bpButton = {
   color: '#fff',
   backgroundColor: BP_COLOR[20],
   '&:hover, &:active': {
-    backgroundColor: BP_COLOR[10],
-  },
+    backgroundColor: BP_COLOR['20'],
+    textDecoration: 'underline'
+  }
   '&:disabled': {
     backgroundColor: BP_COLOR[40],
   },
-};
+}
 
 export const bpTheme = {
   ...defaultTheme,
@@ -29,57 +30,57 @@ export const bpTheme = {
           '&.RaMenuItemLink-active': {
             backgroundColor: BP_COLOR[10],
             color: '#fff',
-            borderLeft: '3px solid #fff',
+            borderLeft: '3px solid #fff'
           },
           '&.RaMenuItemLink-active .RaMenuItemLink-icon': {
-            color: '#fff',
+            color: '#fff'
           },
           '& .RaMenuItemLink-icon': {
-            color: BP_COLOR[30],
-          },
-        },
-      },
+            color: BP_COLOR[30]
+          }
+        }
+      }
     },
     RaLayout: {
       styleOverrides: {
         root: {
           '& .RaLayout-content': {
             marginTop: '1rem',
-            paddingLeft: '0.5rem',
-          },
-        },
-      },
+            paddingLeft: '0.5rem'
+          }
+        }
+      }
     },
     MuiPaper: {
       styleOverrides: {
         elevation1: {
-          boxShadow: 'none',
+          boxShadow: 'none'
         },
         root: {
           border: '1px solid #ebebeb',
-          backgroundClip: 'padding-box',
-        },
-      },
+          backgroundClip: 'padding-box'
+        }
+      }
     },
     RaListToolbar: {
       styleOverrides: {
         root: {
           backgroundColor: '#fff',
-          borderRadius: '0.4rem 0.4rem 0 0',
-        },
-      },
+          borderRadius: '0.4rem 0.4rem 0 0'
+        }
+      }
     },
     RaList: {
       styleOverrides: {
         root: {
-          '& .RaList-main button': bpButton,
-        },
-      },
+          '& .RaList-main button': bpButton
+        }
+      }
     },
     MuiButton: {
       styleOverrides: {
-        root: bpButton,
-      },
-    },
-  },
-};
+        root: bpButton
+      }
+    }
+  }
+}
--- src/security/LoginPage.js
+++ src/security/LoginPage.js
@@ -1,20 +1,21 @@
-import { useReducer, useState, useEffect } from 'react';
-import { Box, Typography, TextField, FormHelperText, Button, useMediaQuery } from '@mui/material';
+import { useReducer, useState, useEffect } from 'react'
+import { Box, Typography, TextField, FormHelperText, Button, useMediaQuery } from '@mui/material'
 
-import { securityApi } from '../providers/api';
-import loginRedirectionUrls from './login-redirection-urls';
-import { redirect } from '../utils/redirect';
-import { v4 as uuidv4 } from 'uuid';
-import { FLEX_CENTER, BP_B_LOGO, LOGIN_FORM, LOGIN_FORM_BUTTON, pinkColor } from './style.js';
+import { onboardingApi, securityApi } from '../providers/api'
+import loginRedirectionUrls from './login-redirection-urls'
+import { redirect } from '../utils/redirect'
+import { v4 as uuidv4 } from 'uuid'
+import { FLEX_CENTER, BP_B_LOGO, LOGIN_FORM, LOGIN_FORM_BUTTON, pinkColor } from './style.js'
+import { BP_COLOR } from '../bpTheme'
 
 const PhoneTextField = ({ formInput: { phone }, handleInput, isFirstAttempt }) => {
-  const [isValidPhone, setIsValidPhone] = useState(true);
+  const [isValidPhone, setIsValidPhone] = useState(true)
   useEffect(() => {
-    const testValidPhone = phone => /\d{10}/.test(phone);
-    setIsValidPhone(testValidPhone(phone));
-  }, [setIsValidPhone, phone]);
+    const testValidPhone = phone => /\d{10}/.test(phone)
+    setIsValidPhone(testValidPhone(phone))
+  }, [setIsValidPhone, phone])
 
-  const isError = !isFirstAttempt && !isValidPhone;
+  const isError = !isFirstAttempt && !isValidPhone
   return (
     <TextField
       id='phone'
@@ -25,33 +26,41 @@ const PhoneTextField = ({ formInput: { phone }, handleInput, isFirstAttempt }) =
       error={isError}
       helperText={isError ? 'Votre numéro doit ressembler à 0611223344' : null}
     />
-  );
-};
+  )
+}
 
 const BpLoginPage = () => {
   // Following reducer technique for update state of the form is taken from
   // https://codesandbox.io/s/react-material-ui-form-submit-v40lz?from-embed=&file=/src/components/MaterialUIFormSubmit.js
   const [formInput, setFormInput] = useReducer((state, newState) => ({ ...state, ...newState }), {
-    phone: '',
-  });
-  const matchMediaQuery = useMediaQuery('(max-width: 898px)');
-  const handleInput = ({ target: { name, value } }) => setFormInput({ [name]: value });
+    phone: ''
+  })
+  const matchMediaQuery = useMediaQuery('(max-width: 898px)')
+  const handleInput = ({ target: { name, value } }) => setFormInput({ [name]: value })
 
-  const [isFirstAttempt, setFirstAttempt] = useState(true);
+  const [isFirstAttempt, setFirstAttempt] = useState(true)
   const initiateAuth = async () => {
     const {
-      data: { redirectionUrl },
+      data: { redirectionUrl }
     } = await securityApi().initiateAuth({
       state: uuidv4(),
       phone: formInput.phone,
-      redirectionStatusUrls: loginRedirectionUrls,
-    });
-    redirect(redirectionUrl);
-  };
+      redirectionStatusUrls: loginRedirectionUrls
+    })
+    redirect(redirectionUrl)
+  }
   const onLogin = () => {
-    setFirstAttempt(false);
-    initiateAuth();
-  };
+    setFirstAttempt(false)
+    initiateAuth()
+  }
+
+  const onRegistration = () => {
+    const initiateOnboarding = async () => {
+      const { data: { redirectionUrl } } = await onboardingApi().initiateOnboarding({ redirectionStatusUrls: loginRedirectionUrls })
+      redirect(redirectionUrl)
+    }
+    initiateOnboarding()
+  }
 
   return (
     <Box sx={FLEX_CENTER}>
@@ -67,13 +76,26 @@ const BpLoginPage = () => {
           <Button id='login' onClick={onLogin} sx={LOGIN_FORM_BUTTON}>
             Se connecter
           </Button>
+          <Button sx={{
+            backgroundColor: 'transparent',
+            color: '#000',
+            textDecoration: 'underline',
+            textTransform: 'none',
+            textAlign: 'left',
+            mt: 2,
+            p: 0,
+            '&:hover': {
+              backgroundColor: 'transparent',
+              color: BP_COLOR[20]
+            }
+          }} onClick={onRegistration}>Pas de compte ? C'est par ici</Button>
         </Box>
       </Box>
-      <Box width={{ md: '60%', sm: '0%', xs: '0%' }} sx={{ ...FLEX_CENTER, bgcolor: pinkColor }}>
+      <Box width={{ md: '60%', sm: '0%', xs: '0%' }}>
         <img src='./bp-logo-full.png' width={600} alt='Bienvenue sur BPartners !' />
       </Box>
     </Box>
-  );
-};
+  )
+}
 
-export default BpLoginPage;
+export default BpLoginPage
