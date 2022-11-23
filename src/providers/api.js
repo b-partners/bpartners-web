da9eab9b8c665c60b8fa4936e42a453f1661bacc
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-11-23 14:48:41 +0300

    fix: style

--- src/providers/api.js
+++ src/providers/api.js
@@ -1,18 +1,10 @@
-import {
-  CustomersApi,
-  FilesApi,
-  OnboardingApi,
-  PayingApi,
-  ProspectingApi,
-  SecurityApi,
-  UserAccountsApi
-} from '../gen/bpClient'
-import authProvider from './auth-provider'
+import { CustomersApi, FilesApi, OnboardingApi, PayingApi, ProspectingApi, SecurityApi, UserAccountsApi } from '../gen/bpClient';
+import authProvider from './auth-provider';
 
-export const securityApi = () => new SecurityApi(authProvider.getCachedAuthConf())
-export const userAccountsApi = () => new UserAccountsApi(authProvider.getCachedAuthConf())
-export const payingApi = () => new PayingApi(authProvider.getCachedAuthConf())
-export const customerApi = () => new CustomersApi(authProvider.getCachedAuthConf())
-export const FileApi = () => new FilesApi(authProvider.getCachedAuthConf())
-export const prospectingApi = () => new ProspectingApi(authProvider.getCachedAuthConf())
-export const onboardingApi = () => new OnboardingApi(authProvider.getCachedAuthConf())
+export const securityApi = () => new SecurityApi(authProvider.getCachedAuthConf());
+export const userAccountsApi = () => new UserAccountsApi(authProvider.getCachedAuthConf());
+export const payingApi = () => new PayingApi(authProvider.getCachedAuthConf());
+export const customerApi = () => new CustomersApi(authProvider.getCachedAuthConf());
+export const FileApi = () => new FilesApi(authProvider.getCachedAuthConf());
+export const prospectingApi = () => new ProspectingApi(authProvider.getCachedAuthConf());
+export const onboardingApi = () => new OnboardingApi(authProvider.getCachedAuthConf());
