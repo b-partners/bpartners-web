872430a4d281a4ea4dcb9f6d856f9f4a4c344c68
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-11-23 15:10:53 +0300

    fix: style

--- src/__tests__/Login.cy.js
+++ src/__tests__/Login.cy.js
@@ -1,49 +1,49 @@
-import { mount } from '@cypress/react'
-import specTitle from 'cypress-sonarqube-reporter/specTitle'
+import { mount } from '@cypress/react';
+import specTitle from 'cypress-sonarqube-reporter/specTitle';
 
-import LoginPage from '../security/LoginPage'
-import LoginSuccessPage from '../security/LoginSuccessPage'
+import LoginPage from '../security/LoginPage';
+import LoginSuccessPage from '../security/LoginSuccessPage';
 
-import { phone1, token1, whoami1 } from './mocks/responses/security-api'
-import * as Redirect from '../utils/redirect'
-import App from 'src/App'
+import { phone1, token1, whoami1 } from './mocks/responses/security-api';
+import * as Redirect from '../utils/redirect';
+import App from 'src/App';
 
 describe(specTitle('Login'), () => {
   beforeEach(() => {
-    cy.stub(Redirect, 'redirect').as('redirect')
-  })
+    cy.stub(Redirect, 'redirect').as('redirect');
+  });
 
   it('MainPage redirects to authUrl on phone submission', () => {
-    mount(<LoginPage />)
-    cy.get('#phone').type(phone1)
+    mount(<LoginPage />);
+    cy.get('#phone').type(phone1);
 
     //TODO: ask backend to fix token.whoami
-    cy.intercept('POST', '/authInitiation', { redirectionUrl: 'https://authUrl.com' }).as('createAuthInitiation')
-    cy.get('#login').click()
-    cy.wait('@createAuthInitiation')
-    cy.get('@redirect').should('have.been.calledOnce')
-  })
+    cy.intercept('POST', '/authInitiation', { redirectionUrl: 'https://authUrl.com' }).as('createAuthInitiation');
+    cy.get('#login').click();
+    cy.wait('@createAuthInitiation');
+    cy.get('@redirect').should('have.been.calledOnce');
+  });
 
   it('SuccessPage redirects to / on valid code', () => {
-    cy.intercept('POST', '/token', token1).as('createToken')
-    cy.intercept('GET', '/whoami', whoami1).as('whoami')
-    mount(<LoginSuccessPage />)
-    cy.wait('@createToken')
-    cy.get('@redirect').should('have.been.calledOnce')
-  })
+    cy.intercept('POST', '/token', token1).as('createToken');
+    cy.intercept('GET', '/whoami', whoami1).as('whoami');
+    mount(<LoginSuccessPage />);
+    cy.wait('@createToken');
+    cy.get('@redirect').should('have.been.calledOnce');
+  });
 
   it('should redirect to LoginPage when not connected', () => {
-    mount(<App />)
-    cy.contains('Se connecter')
-    cy.contains('Bienvenue !')
-  })
+    mount(<App />);
+    cy.contains('Se connecter');
+    cy.contains('Bienvenue !');
+  });
 
   it('MainPage redirects to onboarding url', () => {
-    mount(<LoginPage />)
-    cy.intercept('POST', '/onboardingInitiation', { redirectionUrl: 'https://authUrl.com' }).as('onboardingInitiation')
-    cy.get('#register').contains(`Pas de compte ? C'est par ici`)
-    cy.get('#register').click()
-    cy.wait('@onboardingInitiation')
-    cy.get('@redirect').should('have.been.calledOnce')
-  })
-})
+    mount(<LoginPage />);
+    cy.intercept('POST', '/onboardingInitiation', { redirectionUrl: 'https://authUrl.com' }).as('onboardingInitiation');
+    cy.get('#register').contains(`Pas de compte ? C'est par ici`);
+    cy.get('#register').click();
+    cy.wait('@onboardingInitiation');
+    cy.get('@redirect').should('have.been.calledOnce');
+  });
+});
