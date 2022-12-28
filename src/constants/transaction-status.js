caec05c26dc8933335572961faf0280b27418187
Lou Andria <lou@hei.school>
2022-12-28 16:31:48 +0100

    feat: support for all possible transaction statuses

--- src/constants/transaction-status.js
+++ src/constants/transaction-status.js
@@ -1,4 +1,12 @@
 export const TRANSACTION_STATUSES = {
   PENDING: { label: 'En attente', color: 'orange' },
-  BOOKED: { label: 'Terminée', color: 'green' },
+  UPCOMING: { label: 'En réception', color: 'blue' },
+  BOOKED: { label: 'Acceptée', color: 'green' },
+  REJECTED: { label: 'Rejetée', color: 'red' },
+  UNKNOWN: {
+    // Internally, we use this status to handle e.g. any new status introcuced by backend and not handle yet frontend-side.
+    // Visually, we must avoid displaying to users that status is unknown: we must display re-assuring message instead!
+    label: 'En traitement',
+    color: 'orange',
+  },
 };
