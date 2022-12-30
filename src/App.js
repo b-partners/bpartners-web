b25fe995b9b210001185318bcaaaa935ce60a31f
AmourRamanantsiresy <hei.amour.2@gmail.com>
2022-12-30 09:57:10 +0300

    fix: authentication redirection

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Admin } from '@react-admin/ra-enterprise';
 import { Resource } from '@react-admin/ra-rbac';
 import polyglotI18nProvider from 'ra-i18n-polyglot';
 import frenchMessages from 'ra-language-french';
+import { useEffect } from 'react';
 import { CustomRoutes } from 'react-admin';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import BpErrorPage from './BpErrorPage';
@@ -18,52 +19,51 @@ import { marketplaces } from './operations/marketplaces';
 import products from './operations/products';
 import transactions from './operations/transactions';
 
-import authProvider from './providers/auth-provider.ts';
+import authProvider from './providers/auth-provider';
 import dataProvider from './providers/data-provider';
 import { loginSuccessRelUrl } from './security/login-redirection-urls';
 
 import LoginPage from './security/LoginPage';
 import LoginSuccessPage from './security/LoginSuccessPage';
-import useAuthentication from './utils/useAuthentication';
 import MobileLoginSuccessPage from './security/MobileLoginSuccessPage';
 
-export const BpAdmin = () => (
-  <Admin
-    title='BPartners'
-    authProvider={authProvider}
-    dataProvider={dataProvider}
-    i18nProvider={polyglotI18nProvider(() => frenchMessages, 'fr')}
-    loginPage={false}
-    theme={bpTheme}
-    layout={MyLayout}
-  >
-    <Resource name='transactions' {...transactions} />
-    <Resource name='customers' {...customers} />
-    <Resource name='products' {...products} />
-    <Resource name='invoices' {...invoice} />
-    <Resource name='marketplaces' {...marketplaces} />
-    <Resource name='account' />
+export const BpAdmin = () => {
+  return !authProvider.getCachedWhoami() ? (
+    <Navigate to='/login' />
+  ) : (
+    <Admin
+      title='BPartners'
+      authProvider={authProvider}
+      dataProvider={dataProvider}
+      i18nProvider={polyglotI18nProvider(() => frenchMessages, 'fr')}
+      loginPage={false}
+      theme={bpTheme}
+      layout={MyLayout}
+    >
+      <Resource name='transactions' {...transactions} />
+      <Resource name='customers' {...customers} />
+      <Resource name='products' {...products} />
+      <Resource name='invoices' {...invoice} />
+      <Resource name='marketplaces' {...marketplaces} />
+      <Resource name='account' />
 
-    <CustomRoutes>
-      <Route exact path='/account' element={<account.show />} />
-      <Route exact path='/products' element={<products.list />} />
-      <Route exact path='/configurations' element={<Configuration />} />
-      <Route exact path='/invoice' element={<invoice.list />} />
-      <Route exact path='/error' element={<BpErrorPage />} />
-    </CustomRoutes>
-  </Admin>
-);
+      <CustomRoutes>
+        <Route exact path='/account' element={<account.show />} />
+        <Route exact path='/configurations' element={<Configuration />} />
+        <Route exact path='/error' element={<BpErrorPage />} />
+      </CustomRoutes>
+    </Admin>
+  );
+};
 
 const App = () => {
-  const { isLoading, isAuthenticated } = useAuthentication();
   return (
     <BrowserRouter>
       <Routes>
-        {isLoading && <Route path='*' element={<BpLoading />} />}
         <Route exact path={loginSuccessRelUrl} element={<LoginSuccessPage />} />
-        <Route exact path='/login' element={!isAuthenticated ? <LoginPage /> : <Navigate to='/' />} />
+        <Route exact path='/login' element={<LoginPage />} />
         <Route exact path='/login/mobile/success' element={<MobileLoginSuccessPage />} />
-        <Route exact path='*' element={isAuthenticated ? <BpAdmin /> : <Navigate to='/login' />} />
+        <Route exact path='*' element={<BpAdmin />} />
       </Routes>
     </BrowserRouter>
   );
