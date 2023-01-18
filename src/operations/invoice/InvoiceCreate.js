ab0e64f279d905c3ad31e04f0a25c57933b7c99f
Yume <hei.hajatiana@gmail.com>
2023-01-18 23:07:49 +0300

    fix: make delayInPaymentAllowed required

--- src/operations/invoice/InvoiceCreate.js
+++ src/operations/invoice/InvoiceCreate.js
@@ -123,7 +123,7 @@ const InvoiceCreateOrUpdate = props => {
                   form={form}
                 />
                 <CustomFilledInput
-                  validate={e => e && e >= 0 && e < 31}
+                  validate={d => d && d >= 0 && d < 31}
                   name='delayInPaymentAllowed'
                   label='Délai de retard de payment autorisé (jours)'
                   type='number'
