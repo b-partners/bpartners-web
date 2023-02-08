4ab9f01a58aacded143b559636f9847fdf31274b
Judicael <99041636+JrManjato@users.noreply.github.com>
2023-02-08 13:46:17 +0300

    feat: improve design of the login page

--- src/security/LoginSuccessPage.js
+++ src/security/LoginSuccessPage.js
@@ -1,9 +1,13 @@
 import { useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
 
 import authProvider from '../providers/auth-provider';
 import loginRedirectionUrls from './login-redirection-urls';
 import { redirect } from '../common/utils/redirect';
 import { getUrlParams } from '../common/utils/get-params';
+
+import { FLEX_CENTER } from './style.js';
+import BpBackgroundImage from '../assets/bp-bg-image.png';
 
 const LoginSuccessPage = () => {
   useEffect(() => {
@@ -15,7 +19,29 @@ const LoginSuccessPage = () => {
     login();
   }, []);
 
-  return <p>Vous êtes authentifié ! Vous allez être redirigé vers votre espace professionel...</p>;
+  return (
+    <Box
+      width={{ md: '100%', sm: '0%', xs: '0%' }}
+      sx={{
+        ...FLEX_CENTER,
+        height: '100vh',
+        backgroundImage: `url(${BpBackgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+      }}
+    >
+      <Typography
+        sx={{
+          color: '#F5F5F5',
+          position: 'absolute',
+          bottom: '6.5rem',
+        }}
+      >
+        Vous êtes authentifié ! Vous allez être redirigé vers votre espace professionel...
+      </Typography>
+    </Box>
+  );
 };
 
 export default LoginSuccessPage;
