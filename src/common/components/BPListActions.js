e24cd5fdf0c77c52ef3b3c65fec61714174097cc
Rojotiana Nomena <hei.nomena@gmail.com>
2023-02-15 17:01:06 +0300

    feat: bp custom import

--- src/common/components/BPListActions.js
+++ src/common/components/BPListActions.js
@@ -5,12 +5,13 @@ import { MoreVert as MoreIcon } from '@mui/icons-material';
 import { Box } from '@mui/material';
 
 const BPListActions = props => {
-  const { buttons, hasCreate, hasExport, label, icon } = props;
+  const { buttons, hasCreate, hasExport, importComponent, label, icon } = props;
   return (
     <PopoverButton style={{ marginRight: 5.2 }} icon={icon || <MoreIcon />} label={label || 'Menu'}>
       <Box sx={{ width: '13rem', padding: 0.5, display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
         {hasCreate !== false && <CreateButton data-testId='create-button' label='Créer' sx={{ width: '12rem', margin: 1, paddingBlock: 1 }} />}
         {buttons}
+        {importComponent}
         {hasExport !== false && (
           <ExportButton data-testId='export-button' label='Exporter' sx={{ width: '12rem', marginInline: 1, marginBottom: 1, paddingBlock: 1 }} />
         )}
