9b403b4e2167f89c2d8e2975cdbdf2bfd4a337b0
Judicael <judi.randriamanjato@gmail.com>
2023-05-18 19:55:37 +0300

    feat: put the sign-up form in a new route

---
+++ src/security/SignInForm.js
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FormProvider, useForm } from 'react-hook-form';
+import { Button, CircularProgress, Typography } from '@mui/material';
+import authProvider from '../providers/auth-provider';
+import { redirect } from '../common/utils/redirect';
+import { LOGIN_FORM, LOGIN_FORM_BUTTON } from './style';
+import { handleSubmit } from '../common/utils';
+import { BpFormField } from '../common/components';
+import { BP_COLOR } from '../bp-theme';
+import CompletePasswordPage from './CompletePasswordPage';
+
+const SignInForm = () => {
+  const formState = useForm({ mode: 'all', defaultValues: { username: '', password: '' } });
+  const [isLoading, setLoading] = useState(false);
+
+  const login = formState.handleSubmit(async loginState => {
+    setLoading(true);
+    const redirectionUrl = await authProvider.login(loginState);
+    redirect(redirectionUrl);
+  });
+
+  const navigate = useNavigate();
+
+  return (
+    <FormProvider {...formState}>
+      <form style={LOGIN_FORM} onSubmit={handleSubmit(login)}>
+        <img src='./laborer.webp' width={50} height={50} alt='Bienvenue sur BPartners !' />
+        <Typography variant='h5' gutterBottom mt={1}>
+          Bienvenue !
+        </Typography>
+        <BpFormField name='username' label='Votre email' />
+        <BpFormField name='password' type='password' label='Mot de passe' />
+        <Button
+          id='login'
+          endIcon={isLoading && <CircularProgress size={20} color='inherit' />}
+          disabled={isLoading}
+          style={{ marginTop: '0.5rem' }}
+          type='submit'
+          sx={LOGIN_FORM_BUTTON}
+        >
+          Se connecter
+        </Button>
+        <Button
+          id='register'
+          sx={{
+            backgroundColor: 'transparent',
+            color: '#000000',
+            textTransform: 'none',
+            textAlign: 'left',
+            mt: 2,
+            p: 0,
+            '&:hover': {
+              backgroundColor: 'transparent',
+              textDecoration: 'underline',
+              color: BP_COLOR[20],
+            },
+          }}
+          onClick={() => {
+            navigate('/sign-up');
+          }}
+        >
+          <Typography variant='h7' gutterBottom mt={1}>
+            Pas de compte ? <b>C'est par ici</b>
+          </Typography>
+        </Button>
+      </form>
+    </FormProvider>
+  );
+};
+
+const ResponsiveCompletePassword = () => <CompletePasswordPage style={{ backgroundImage: 'inherit' }} />;
+export const PasswordChangeableLogin = () => {
+  return authProvider.isTemporaryPassword() ? <ResponsiveCompletePassword /> : <SignInForm />;
+};
