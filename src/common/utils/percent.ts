be1d41c8f0752f9d589f60cd6b3c39c4a6e159e6
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-05-31 09:26:56 +0300

    fix: product tva

--- src/common/utils/percent.ts
+++ src/common/utils/percent.ts
@@ -1,1 +1,2 @@
-export const prettyPrintPercentMinors = (value: number): string => ((value / 100).toFixed(2) + ' %').replace('.', ',');
+export const prettyPrintPercentMinors = (value: number, map?: boolean): string =>
+  map !== false ? ((value / 100).toFixed(2) + ' %').replace('.', ',') : `${value} %`;
