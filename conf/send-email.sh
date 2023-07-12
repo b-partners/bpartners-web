27ecb05939f231f556e1d9a52bba672125e18394
onitsiky <hei.onitsiky@gmail.com>
2023-07-12 15:25:28 +0300

    infra: upload cypress test videos to s3 on test failure

--- conf/send-email.sh
+++ conf/send-email.sh
@@ -4,6 +4,6 @@ while IFS= read -r email; do
   aws ses send-email \
       --from bpartners.artisans@gmail.com \
       --to "$email" \
-      --message file://.conf/email-template.json \
+      --message file://conf/email-template.json \
       --profile "$1"
-done < .conf/recipients.txt
+done < conf/it-subscribers.txt
