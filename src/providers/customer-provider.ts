6ab47427b2f47f8ebd8ab2946fa21ce76cb7edbf
Lou Andria <lou@hei.school>
2023-07-18 20:18:43 +0200

    fix: status is NOT part of customer payload

--- src/providers/customer-provider.ts
+++ src/providers/customer-provider.ts
@@ -21,7 +21,7 @@ export const customerProvider: BpDataProviderType = {
     const { accountId } = getCached.userInfo();
     let customers = [];
     if (resource.config) {
-      const {config, headers, request, data, ...customer} = resource;
+      const {config, headers, request, data, status, ...customer} = resource;
       customers.push(customer);
     } else {
       customers.push(resource);
@@ -35,7 +35,7 @@ export const customerProvider: BpDataProviderType = {
     const { accountId } = getCached.userInfo();
     let customers = [];
     if (resource.config) {
-      const {config, headers, request, data, ...customer} = resource;
+      const {config, headers, request, data, status, ...customer} = resource;
       customers.push(customer);
     } else {
       customers.push(resource);
