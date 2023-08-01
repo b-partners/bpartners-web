54372b0133007018f67e09df5f95ed9f0b614956
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-08-01 11:34:01 +0300

    test(Invoice IT): payment regulation comment

--- src/__tests__/it/Invoice.it.cy.js
+++ src/__tests__/it/Invoice.it.cy.js
@@ -17,6 +17,7 @@ describe(specTitle('Invoice'), () => {
     cy.get('[name="create-draft-invoice"]').click();
 
     const ref = 'it-' + uuid().substring(0, 7);
+    const comment = `Test comment for ${ref}`;
     cy.get('[data-testid="ref-field-input"]').click().clear().type(ref);
     cy.get('[data-testid="sendingDate-field-input"]').click().clear().type('2023-06-26');
 
@@ -31,10 +32,29 @@ describe(specTitle('Invoice'), () => {
     cy.contains('Bon pour accord');
 
     cy.intercept('GET', '/accounts/7efea565-bc2b-463f-b219-ef695c4acdc8/invoices?page=1&pageSize=15&status=DRAFT').as('getdrafts');
+
+    // test payment regulation comment
+    cy.get('[data-testid="payment-regulation-checkbox-id"] > .PrivateSwitchBase-input').click();
+    cy.get('[data-testid="invoice-Acompte-accordion"]').click();
+    cy.get('#form-create-regulation-id').click();
+    cy.get("input[name='comment']").clear().type(comment);
+    cy.get('#form-regulation-save-id').click();
+    cy.contains(comment);
+
     cy.get('#form-save-id').click();
 
     const timeout = 10_000;
     cy.wait('@getdrafts', { timeout: timeout });
+
+    cy.contains(ref).click();
+    cy.get('[data-testid="invoice-Acompte-accordion"]').click();
+    cy.get(':nth-child(1) > .MuiPaper-root > .MuiCardActions-root > .MuiBox-root > :nth-child(1)').click();
+    cy.contains(comment);
+
+    cy.get('#form-save-id').click();
+
+    cy.wait('@getdrafts', { timeout: timeout });
+
     cy.intercept('GET', '/accounts/7efea565-bc2b-463f-b219-ef695c4acdc8/invoices?page=1&pageSize=15&status=PROPOSAL').as('getproposals');
     cy.get(`[data-testid="invoice-conversion-PROPOSAL-BROUILLON-${ref}"]`).click();
     cy.contains('Brouillon transformé en devis', { timeout: timeout });
