efc1aca8af8a8d1b22b5aebd20d2b83fed82a86f
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-08-04 15:29:08 +0300

    chore: client selection filter backend side

--- src/operations/invoice/components/ClientSelection.js
+++ src/operations/invoice/components/ClientSelection.js
@@ -1,33 +1,46 @@
-import { TextField, Autocomplete } from '@mui/material';
-import { useGetList } from 'react-admin';
+import { Autocomplete, TextField } from '@mui/material';
 import { useFormContext, useWatch } from 'react-hook-form';
+import { useAutocomplete } from '../../../common/hooks/use-autocomplete';
+import { customerProvider } from '../../../providers';
 
 const getClientName = customer => (customer && customer.lastName && customer.firstName ? `${customer.lastName} ${customer.firstName}` : '');
 
 export const ClientSelection = props => {
   const { name, label, sx } = props;
   const { setValue } = useFormContext();
-
   const client = useWatch({ name });
 
-  const { data: clients = [], isLoading, error } = useGetList('customers', { pagination: { page: 1, perPage: 500 } });
-  const checkError = !client || error;
+  const fetcher = async q => await customerProvider.getList(1, 500, { customerListSearch: q });
+
+  const { options, loading, onChange, query, onblur } = useAutocomplete({
+    defaultValue: client,
+    fetcher,
+    getLabel: getClientName,
+  });
+  const checkError = !client;
   const errorProps = checkError && { error: true, helperText: 'Ce champ est requis' };
-  const handleChange = (_event, value) => setValue(name, value);
+  const handleChange = (_event, value) => {
+    setValue(name, value);
+    onChange(getClientName(value));
+  };
 
   return (
     <Autocomplete
       disablePortal
+      autoComplete
+      filterOptions={e => e}
+      loading={loading}
       noOptionsText='Aucun élément'
       clearIcon={false}
-      value={client || {}}
-      loading={isLoading}
+      value={client || null}
       getOptionLabel={getClientName}
-      options={clients}
+      options={options}
+      inputValue={query}
+      inputMode='text'
       onChange={handleChange}
       sx={{ width: 300, marginBlock: '3px', ...sx }}
       data-testid='invoice-client-selection'
-      renderInput={params => <TextField {...errorProps} {...params} label={label} />}
+      renderInput={params => <TextField {...errorProps} {...params} onBlur={onblur} value={query} onChange={q => onChange(q.target.value)} label={label} />}
     />
   );
 };
