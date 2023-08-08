1af22a7f4b53ca6518f745d4b2f9197cd0a6464c
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-08-08 14:45:36 +0300

    chore: beautify prospect style

--- src/operations/prospects/ProspectsList.js
+++ src/operations/prospects/ProspectsList.js
@@ -94,15 +94,22 @@ const ProspectColumn = props => {
   const { title, list, color } = props;
   return (
     <Grid item xs={4}>
-      <Card sx={{ minWidth: 275, height: '89vh', bgcolor: BP_COLOR['solid_grey'] }}>
+      <Card sx={{ minWidth: 275, height: '89vh', boxShadow: '2px 2px 10px rgba(0, 0, 0, 0.1) !important', border: 'none' }}>
         <CardContent>
           <Stack spacing={3}>
-            <Paper elevation={3} sx={{ textAlign: 'center', py: 1 }}>
+            <Box sx={{ textAlign: 'center', py: 1 }}>
               <Typography variant='h6' color={color}>
                 {title}
               </Typography>
-            </Paper>
-            <Stack spacing={1} sx={{ overflowY: 'scroll', height: '75vh', '&::-webkit-scrollbar': { display: 'none' } }}>
+            </Box>
+            <Stack
+              spacing={1}
+              sx={{
+                overflowY: 'scroll',
+                height: '75vh',
+                '&::-webkit-scrollbar': { display: 'none' },
+              }}
+            >
               {list && list.map(item => <ProspectItem key={`prospect-item-${item.id}`} prospect={item} />)}
             </Stack>
           </Stack>
