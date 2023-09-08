7141108789fd360c228725a3a98a8d2593c15dea
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-09-08 14:31:06 +0300

    chore: invoice list filter

--- src/providers/invoice-provider.ts
+++ src/providers/invoice-provider.ts
@@ -1,13 +1,16 @@
-import { ArchiveStatus, InvoiceStatus } from 'bpartners-react-client';
+import { ArchiveStatus } from 'bpartners-react-client';
 import { invoiceMapper } from 'src/operations/invoice/utils/invoice-utils';
 import { asyncGetAccountId, getCached, payingApi } from '.';
 import { BpDataProviderType } from './bp-data-provider-type';
 
 export const invoiceProvider: BpDataProviderType = {
-  getList: async function (page: number, perPage: number, filter: any): Promise<any[]> {
+  getList: async function (page: number, perPage: number, filters = {}): Promise<any[]> {
+    const { invoiceTypes, invoiceListSearch } = filters;
+
     const accountId = await asyncGetAccountId();
-    const invoiceTypes: Array<InvoiceStatus> = filter.invoiceTypes;
-    return (await payingApi().getInvoices(accountId, page, perPage, undefined, invoiceTypes, ArchiveStatus.ENABLED)).data;
+    const searchValues = ((invoiceListSearch as string) || '').split(' ');
+
+    return (await payingApi().getInvoices(accountId, page, perPage, undefined, invoiceTypes, ArchiveStatus.ENABLED, undefined, searchValues)).data;
   },
   getOne: async function (invoiceId: string): Promise<any> {
     const { accountId } = getCached.userInfo();
