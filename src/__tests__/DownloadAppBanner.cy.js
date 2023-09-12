bcf5782c8d801fc77a9d700f7823fbbb9f8e9e67
mohammediche <mohammediche@gmail.com>
2023-09-12 10:35:58 +0200

    test: fix DownloadAppBanner test

--- src/__tests__/DownloadAppBanner.cy.js
+++ src/__tests__/DownloadAppBanner.cy.js
@@ -2,7 +2,7 @@ import { mount } from '@cypress/react';
 import specTitle from 'cypress-sonarqube-reporter/specTitle';
 import App from 'src/App';
 
-describe(specTitle(''), { viewportWidth: 1000 }, () => {
+describe(specTitle('download app banner'), { viewportWidth: 1000 }, () => {
   it('show download app banner below 1000px', () => {
     mount(<App />);
     cy.contains('Pour plus de confort, télécharger notre application mobile !');
