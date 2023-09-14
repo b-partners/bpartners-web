e6e1e64ee80ccaa5e5bf62952e5e98b9c4791b27
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-09-14 15:56:53 +0300

    chore: calendar show

--- src/__tests__/Calendar.cy.js
+++ src/__tests__/Calendar.cy.js
@@ -2,20 +2,11 @@ import { mount } from '@cypress/react';
 import specTitle from 'cypress-sonarqube-reporter/specTitle';
 import App from 'src/App';
 import * as Redirect from '../common/utils';
+import { calendarEvents, calendars, redirectionUrl } from './mocks/responses/calendar-api';
 
 const calendarTokenNotValid = () => {
   cy.intercept('GET', '/users/mock-user-id1/calendars', req => req.reply({ statusCode: 403 }));
-  cy.intercept('POST', '/users/mock-user-id1/calendars/oauth2/consent', req =>
-    req.reply({
-      statusCode: 200,
-      body: {
-        redirectionStatusUrls: {
-          successUrl: 'dummy',
-          failureUrl: 'dummy',
-        },
-      },
-    })
-  );
+  cy.intercept('POST', '/users/mock-user-id1/calendars/oauth2/consent', req => req.reply(redirectionUrl));
   mount(<App />);
   cy.get("[name='calendar']").click();
   cy.contains('Me synchroniser avec mon agenda google');
@@ -24,19 +15,11 @@ const calendarTokenNotValid = () => {
 };
 
 const calendar = () => {
-  cy.intercept('GET', '/users/mock-user-id1/calendars', [{ summary: 'holydays', id: 'holydays-calendar-id', permission: 'READER' }]);
-  cy.intercept('GET', '/users/mock-user-id1/calendars/holydays-calendar-id/events**', [
-    {
-      summary: 'Event for today',
-      organizer: 'Me',
-      location: 'Antananarivo',
-      from: new Date(),
-      id: 'today-event-id',
-      to: new Date(new Date().getTime() + 60 * 1000),
-      participants: ['participants', 'participants'],
-      updatedAt: new Date(),
-    },
-  ]);
+  const now = new Date('2023-01-01');
+  // set date global date 2023-01-01
+  cy.clock(now);
+  cy.intercept('GET', '/users/mock-user-id1/calendars', calendars);
+  cy.intercept('GET', '/users/mock-user-id1/calendars/holydays-calendar-id/events**', calendarEvents);
 
   mount(<App />);
   cy.get("[name='calendar']").click();
@@ -44,6 +27,10 @@ const calendar = () => {
   cy.contains('Mois');
   cy.contains('Semaine');
   cy.contains('Jour');
+  cy.contains('Event for today');
+
+  //restore the current date
+  cy.clock().invoke('restore');
 };
 
 describe(specTitle('Calendar'), () => {
@@ -53,5 +40,5 @@ describe(specTitle('Calendar'), () => {
   });
 
   it('Calendar when token is not valid', calendarTokenNotValid);
-  it('Should test calendar', calendar);
+  it.only('Should test calendar', calendar);
 });
--- src/providers/mappers/calendar-event-mapper.ts
+++ src/providers/mappers/calendar-event-mapper.ts
@@ -17,8 +17,8 @@ export const calendarEventMapper = {
     return {
       id,
       title: summary,
-      start: from.toISOString(),
-      end: to.toISOString(),
+      start: from as any,
+      end: to as any,
       metadata: {
         location,
         organizer,
