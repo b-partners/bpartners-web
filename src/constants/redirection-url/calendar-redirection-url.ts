28f7dd14d5fde2a712e148f90451dc8834969c07
Lou <lou@bpartners.app>
2023-10-09 15:03:04 +0200

    fix: use window.location.origin for calendar.redirectionUrls

--- src/constants/redirection-url/calendar-redirection-url.ts
+++ src/constants/redirection-url/calendar-redirection-url.ts
@@ -2,8 +2,8 @@ import { CalendarAuth, CalendarConsentInit } from 'bpartners-react-client';
 
 export const calendarRedirectionUrls: CalendarConsentInit = {
   redirectionStatusUrls: {
-    failureUrl: `${process.env.REACT_APP_URL}/calendar-sync/failed`,
-    successUrl: `${process.env.REACT_APP_URL}/calendar-sync`,
+    failureUrl: `${window.location.origin}/calendar-sync/failed`,
+    successUrl: `${window.location.origin}/calendar-sync`,
   },
 };
 
