9f2e09f95c8925b4557fa46a6c598aaf7f0abe66
mohammediche <80341619+mohammediche@users.noreply.github.com>
2023-10-12 09:50:57 +0200

    feat: prospect popup dynamic button text (#573)
    * chore: make popup button text dynamic
    
    * chore: data manipulation with the store
    
    * test: test fix Prospects.cy
    
    * test: fix test style
    
    * fix: Typo fix
    
    * chore: use conditional rendering instead of ternary operator

---
+++ src/operations/prospects/components/ProspectDialogActions.js
@@ -0,0 +1,63 @@
+import { Button, DialogActions, CircularProgress } from '@mui/material';
+import { useProspectContext } from 'src/common/store/Prospect-store';
+
+const ProspectDialogActions = ({ prospectStatus, close, prospectFeedback, saveOrUpdateProspectSubmit }) => {
+  const { loading, selectedStatus } = useProspectContext();
+  const shouldRenderButton = text => {
+    return (
+      <Button onClick={saveOrUpdateProspectSubmit} disabled={loading} startIcon={loading && <CircularProgress color='inherit' size={18} />}>
+        {text}
+      </Button>
+    );
+  };
+
+  const renderCancelButton = () => {
+    return <Button onClick={close}>Annuler</Button>;
+  };
+
+  return (
+    <DialogActions>
+      {prospectStatus === 'TO_CONTACT' ? (
+        <>
+          {renderCancelButton()}
+          {prospectFeedback && prospectFeedback === 'NOT_INTERESTED'
+            ? shouldRenderButton('Abandonner ce prospect')
+            : selectedStatus === 'CONTACTED'
+            ? shouldRenderButton('Réserver ce prospect')
+            : selectedStatus === 'CONVERTED'
+            ? shouldRenderButton('Transformer ce prospect en client')
+            : shouldRenderButton('Valider')}
+        </>
+      ) : prospectStatus === 'CONTACTED' ? (
+        <>
+          {renderCancelButton()}
+          {prospectFeedback && prospectFeedback === 'PROPOSAL_DECLINED'
+            ? shouldRenderButton('Abandonner ce prospect')
+            : selectedStatus === 'TO_CONTACT'
+            ? shouldRenderButton('Libérer ce prospect')
+            : selectedStatus === 'CONVERTED'
+            ? shouldRenderButton('Transformer ce prospect en client')
+            : shouldRenderButton('Valider')}
+        </>
+      ) : prospectStatus === 'CONVERTED' ? (
+        <>
+          {renderCancelButton()}
+          {prospectFeedback && prospectFeedback === 'PROPOSAL_DECLINED'
+            ? shouldRenderButton('Abandonner ce prospect')
+            : selectedStatus === 'TO_CONTACT'
+            ? shouldRenderButton('Libérer ce client')
+            : selectedStatus === 'CONTACTED'
+            ? shouldRenderButton('Remettre ce client en prospect')
+            : shouldRenderButton('Valider')}
+        </>
+      ) : (
+        <>
+          {renderCancelButton()}
+          <Button onClick={saveOrUpdateProspectSubmit}>Valider</Button>
+        </>
+      )}
+    </DialogActions>
+  );
+};
+
+export default ProspectDialogActions;
