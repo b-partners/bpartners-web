7eee119423fead2ec6bd9ea85027d9619cac11c3
mohammediche <80341619+mohammediche@users.noreply.github.com>
2024-05-30 17:38:35 +0200

    feat: change image source

--- src/operations/annotator/AnnotatorComponent.tsx
+++ src/operations/annotator/AnnotatorComponent.tsx
@@ -1,42 +1,53 @@
 import { AnnotatorCanvas } from '@bpartners/annotator-component';
-import { AreaPictureMapLayer } from '@bpartners/typescript-client';
+import { AreaPictureMapLayer, ZoomLevel } from '@bpartners/typescript-client';
 import { Box, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useNotify } from 'react-admin';
+import BpSelect from 'src/common/components/BpSelect';
 import { useCanvasAnnotationContext } from 'src/common/store/annotator/Canvas-annotation-store';
 import { getUrlParams, parseUrlParams } from 'src/common/utils';
 import { ZOOM_LEVEL } from 'src/constants/zoom-level';
 import { annotatorProvider } from 'src/providers/annotator-provider';
-import SelectZoomLevel from './components/SelectZoomLevel';
 
-const AnnotatorComponent = ({ allowAnnotation = true, poly_gone, allowSelectZoomLevel = true, width }: any) => {
+const AnnotatorComponent = ({ allowAnnotation = true, poly_gone, allowSelect = true, width }: any) => {
   const { polygons, updatePolygonList } = useCanvasAnnotationContext();
   const { pictureId, prospectId, fileId } = parseUrlParams();
-  const [newZoomLevel, setNewZoomLevel] = useState('HOUSES_0');
+  const [newZoomLevel, setNewZoomLevel] = useState<ZoomLevel>(ZoomLevel.HOUSES_0);
   const [newzoomLevelAsNumber, setNewZoomLevelAsNumber] = useState(20);
   const [fileInfo, setFileInfo] = useState({ filename: '', address: '' });
-  const [loading, setLoading] = useState(false);
-  const [currentlayer, setCurrentlayer] = useState<AreaPictureMapLayer>({});
+  const [loading, setLoading] = useState({
+    zoomLvl: false,
+    layer: false,
+  });
+  const [layer, setLayer] = useState<AreaPictureMapLayer>({});
+  const [otherLayers, setOtherLayers] = useState([]);
+  const [layerChanging, setLayerChanging] = useState(false);
   const notify = useNotify();
 
   useEffect(() => {
     annotatorProvider.getAreaPictureById(pictureId).then(pictureDetail => {
-      const { filename, address, zoom, actualLayer } = pictureDetail;
-      if (allowSelectZoomLevel) {
+      const { filename, address, zoom, actualLayer, otherLayers } = pictureDetail;
+
+      if (allowSelect) {
         setNewZoomLevel(zoom.level);
         setNewZoomLevelAsNumber(zoom.number);
       }
+      //
+      setLayer(actualLayer);
+      setOtherLayers(otherLayers);
       setFileInfo({ filename, address });
-      setCurrentlayer(actualLayer);
-      setLoading(false);
+      setLoading({
+        zoomLvl: false,
+        layer: false,
+      });
     });
-  }, [pictureId, newZoomLevel, allowSelectZoomLevel]);
+  }, [pictureId, newZoomLevel, allowSelect, layerChanging]);
 
   const handleZoomLvl = async (e: any) => {
     const selectedZoom = ZOOM_LEVEL.find(level => level.value === e.target.value);
 
     try {
-      setLoading(true);
+      setLoading(prev => ({ ...prev, zoomLvl: true }));
       await annotatorProvider.getPictureFormAddress(pictureId, {
         address: fileInfo.address,
         fileId,
@@ -49,13 +60,59 @@ const AnnotatorComponent = ({ allowAnnotation = true, poly_gone, allowSelectZoom
     } catch (error) {
       notify('messages.global.error', { type: 'error' });
     } finally {
-      setLoading(false);
+      setLoading(prev => ({ ...prev, zoomLvl: false }));
+    }
+  };
+
+  const handleLayerChanger = async (e: any) => {
+    const selectedLayer = otherLayers.find(layer => layer.name === e.target.value);
+
+    try {
+      setLoading(prev => ({ ...prev, layer: true }));
+      await annotatorProvider.getPictureFormAddress(pictureId, {
+        address: fileInfo.address,
+        fileId,
+        filename: fileInfo.filename,
+        prospectId,
+        zoomLevel: newZoomLevel,
+        layerId: selectedLayer.id,
+      });
+
+      setLayer(selectedLayer);
+      setLayerChanging(!layerChanging);
+    } catch (error) {
+      notify('messages.global.error', { type: 'error' });
+    } finally {
+      setLoading(prev => ({ ...prev, layer: false }));
     }
   };
 
   return (
-    <Box width='100%' height='580px' position='relative' sx={{ mb: 4 }}>
-      {allowSelectZoomLevel && <SelectZoomLevel newZoomLevel={newZoomLevel} handleZoomLvl={handleZoomLvl} loading={loading} />}
+    <Box width='100%' height='580px' position='relative'>
+      {allowSelect && (
+        <>
+          <BpSelect
+            value={newZoomLevel}
+            handleChange={handleZoomLvl}
+            loading={loading.zoomLvl}
+            options={ZOOM_LEVEL}
+            getOptionKey={(option: any) => option.lvl}
+            getOptionValue={(option: any) => option.value}
+            getOptionLabel={(option: any) => option.label}
+            label='Niveau de zoom'
+          />
+          <BpSelect
+            value={layer.name || ''}
+            handleChange={handleLayerChanger}
+            loading={loading.layer}
+            options={otherLayers}
+            getOptionKey={(option: AreaPictureMapLayer) => option.id}
+            getOptionValue={(option: AreaPictureMapLayer) => option.name}
+            getOptionLabel={(option: AreaPictureMapLayer) => `${option.name} ${option.year} ${option.precisionLevelInCm}cm`}
+            label="Source d'image"
+          />
+        </>
+      )}
       {fileInfo.filename && (
         <AnnotatorCanvas
           allowAnnotation={allowAnnotation}
@@ -72,10 +129,10 @@ const AnnotatorComponent = ({ allowAnnotation = true, poly_gone, allowSelectZoom
           zoom={newzoomLevelAsNumber}
         />
       )}
-      {Object.keys(currentlayer).length > 0 && (
+      {Object.keys(layer).length > 0 && (
         <Box sx={{ textAlign: 'center', p: 1, border: '1px solid #ebebeb' }}>
           <Typography variant='body2' style={{ fontWeight: 'bold' }}>
-            Source de l'image: {currentlayer.name}, {currentlayer.precisionLevelInCm}cm, {currentlayer.year}
+            Source de l'image: {layer.name}, {layer.precisionLevelInCm}cm, {layer.year}
           </Typography>
         </Box>
       )}
