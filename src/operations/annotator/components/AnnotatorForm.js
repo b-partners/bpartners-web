17b9430f7e3dbb8f4af9dab3d386de58d7d15c4d
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-06-05 16:18:52 +0300

    feat: add missing inputs

--- src/operations/annotator/components/AnnotatorForm.js
+++ src/operations/annotator/components/AnnotatorForm.js
@@ -1,10 +1,12 @@
 import { Box, Typography } from '@mui/material';
+import { useMemo } from 'react';
 import { SelectInput, TextInput } from 'react-admin';
-import { BpNumberField } from 'src/common/components';
-import { covering } from 'src/constants';
+import { covering, wear } from 'src/constants';
 import SlopeSelect from './SlopeSelect';
 
 const AnnotatorForm = ({ index, surface }) => {
+  const percentagesLevel = useMemo(() => new Array(11).fill(1).map((e, k) => k * 10), [])
+
   return (
     <Box sx={{ p: 2 }}>
       <div style={{ display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
@@ -16,7 +18,9 @@ const AnnotatorForm = ({ index, surface }) => {
         </Box>
         <SelectInput name={`${index}.covering`} source='covering' label='Revêtement' choices={covering} alwaysOn resettable />
         <SlopeSelect name={`${index}.slope`} />
-        <BpNumberField name={`${index}.wearLevel`} source='wearLevel' label="Taux d'usure" />
+        <SelectInput name={`${index}.wear`} source='wear' label='Usure' choices={wear} alwaysOn resettable />
+        <SelectInput name={`${index}.wearLevel`} source='wearLevel' label="Taux d'usure" choices={percentagesLevel} alwaysOn resettable />
+        <SelectInput name={`${index}.moldRate`} source='moldRate' label="Taux de moisissure" choices={percentagesLevel} alwaysOn resettable />
         <TextInput name={`${index}.obstacle`} source='obstacle' label='Obstacle' />
         <TextInput name={`${index}.comment`} source='comment' label='Commentaire' multiline />
       </div>
