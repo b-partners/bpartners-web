5864b6f57cc790a2b04876411acb7aa45668d862
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-06-06 15:30:54 +0300

    feat: implement geo referencer provider

--- src/providers/get-referencer-provider.ts
+++ src/providers/get-referencer-provider.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { ReferencerGeoJSON } from 'src/operations/annotator';
 
 export const getReferencerProvider = {
-  getCoordinatesToPx() {
-    axios.put();
+  async getCoordinatesToPx(geojson: ReferencerGeoJSON) {
+    const { data } = await axios.put('https://referencer-test.azurewebsites.net/api/converter', geojson);
+    return data;
   },
 };
