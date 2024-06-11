cb9e62f76c7b51395dd769981e5731eed6421da6
RickaPrincy <rckprincy@gmail.com>
2024-06-11 15:34:20 +0300

    test: reorder test prospect test

--- src/__tests__/Prospects.cy.js
+++ src/__tests__/Prospects.cy.js
@@ -5,7 +5,7 @@ import App from 'src/App';
 import * as Redirect from '../common/utils';
 import { accountHolders1, accounts1 } from './mocks/responses/account-api';
 import { getInvoices } from './mocks/responses/invoices-api';
-import { contactedProspect, prospects } from './mocks/responses/prospects-api';
+import { prospects } from './mocks/responses/prospects-api';
 import { whoami1 } from './mocks/responses/security-api';
 
 describe(specTitle('Prospects'), () => {
@@ -27,37 +27,6 @@ describe(specTitle('Prospects'), () => {
     });
 
     cy.stub(Redirect, 'redirect').as('redirect');
-  });
-
-  it('are displayed', () => {
-    cy.intercept('GET', `/accountHolders/${accountHolders1[0].id}/prospects`, prospects).as('getProspects');
-
-    mount(<App />);
-    cy.wait('@getUser1');
-    cy.get('[name="prospects"]').click();
-
-    cy.wait('@getProspects');
-
-    cy.contains('À contacter');
-    cy.contains('Contactés');
-    cy.contains('Convertis');
-
-    cy.contains(/John Doe/gi);
-    cy.contains('johnDoe@gmail.com');
-    cy.contains('+261340465338');
-    cy.contains('30 Rue de la Montagne Sainte-Genevieve');
-    cy.contains('Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
-    cy.contains('10.00');
-    cy.contains('6.00');
-
-    // test filter prospect by name
-    const filterName = 'to search';
-    cy.intercept('GET', '/accountHolders/mock-accountHolder-id1/prospects?name=to+search', req => {
-      expect(req.query.name).eq(filterName);
-      req.reply([contactedProspect, ...prospects.slice(1)]);
-    }).as('filterProspect');
-    cy.get("[data-testid='prospect-filter']").type(filterName);
-    cy.wait('@filterProspect');
   });
 
   it('should render the appropriate button', () => {
@@ -148,33 +117,6 @@ describe(specTitle('Prospects'), () => {
     cy.contains('Annuler').click();
   });
 
-  it('change prospecting perimeter', () => {
-    cy.intercept('GET', `/accountHolders/${accountHolders1[0].id}/prospects`, []).as('getProspects1');
-    cy.intercept('PUT', `/users/${whoami1.user.id}/accounts/${accounts1[0].id}/accountHolders/${accountHolders1[0].id}/globalInfo`, req => {
-      expect(req.body.contactAddress.prospectingPerimeter).to.deep.eq(5);
-
-      req.reply(accountHolders1[0]);
-    }).as('updateProspectingPerimeter');
-
-    mount(<App />);
-    cy.wait('@getUser1');
-    cy.get('[name="prospects"]').click();
-    cy.get('[data-cy="configuration-tab"]').click();
-
-    cy.contains('Configurer le périmètre de prospection.');
-    cy.contains('0 km');
-    cy.contains('4');
-    cy.contains('10 km');
-
-    cy.get('[data-testid="perimeterSlider"]').invoke('val', 5).trigger('change').click({ force: true });
-
-    cy.contains('5');
-
-    cy.wait('@updateProspectingPerimeter');
-
-    cy.contains('Changement enregistré');
-  });
-
   it('should show empty list', () => {
     cy.intercept('GET', `/accountHolders/${accountHolders1[0].id}/prospects`, []).as('getProspects1');
     mount(<App />);
@@ -200,5 +142,32 @@ describe(specTitle('Prospects'), () => {
     cy.get('[data-cy="evaluate-prospect"]').click();
 
     cy.get('@redirect').should('have.been.calledOnce');
+  });
+
+  it('change prospecting perimeter', () => {
+    cy.intercept('GET', `/accountHolders/${accountHolders1[0].id}/prospects`, []).as('getProspects1');
+    cy.intercept('PUT', `/users/${whoami1.user.id}/accounts/${accounts1[0].id}/accountHolders/${accountHolders1[0].id}/globalInfo`, req => {
+      expect(req.body.contactAddress.prospectingPerimeter).to.deep.eq(5);
+
+      req.reply(accountHolders1[0]);
+    }).as('updateProspectingPerimeter');
+
+    mount(<App />);
+    cy.wait('@getUser1');
+    cy.get('[name="prospects"]').click();
+    cy.get('[data-cy="configuration-tab"]').click();
+
+    cy.contains('Configurer le périmètre de prospection.');
+    cy.contains('0 km');
+    cy.contains('4');
+    cy.contains('10 km');
+
+    cy.get('[data-testid="perimeterSlider"]').invoke('val', 5).trigger('change').click({ force: true });
+
+    cy.contains('5');
+
+    cy.wait('@updateProspectingPerimeter');
+
+    cy.contains('Changement enregistré');
   });
 });
