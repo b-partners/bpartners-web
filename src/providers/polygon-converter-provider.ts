01dc5064024cd9ac5fde30aa690f71d80c0c2c05
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-06-12 10:47:45 +0300

    feat: use move pixel converter url in env variable

--- src/providers/polygon-converter-provider.ts
+++ src/providers/polygon-converter-provider.ts
@@ -3,7 +3,7 @@ import { ConverterPayloadGeoJSON, ConverterResultGeoJSON } from 'src/operations/
 
 export const polygonConverterProvider = {
   async coordinatesToPixel(geojson: ConverterPayloadGeoJSON): Promise<ConverterResultGeoJSON> {
-    const { data } = await axios.post('https://referencer-test.azurewebsites.net/api/converter', geojson);
+    const { data } = await axios.post(process.env.REACT_APP_ANNOTATOR_PIXEL_CONVERTER_API_URL, geojson);
     return data;
   },
 };
