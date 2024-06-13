f4cfb95804bd2b1c710b4d0747a37a05e2e4aee6
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-06-13 12:14:19 +0300

    test: fix invoice archive test

--- src/__tests__/InvoiceArchive.cy.js
+++ src/__tests__/InvoiceArchive.cy.js
@@ -28,7 +28,7 @@ describe(specTitle('Invoice'), () => {
         getInvoices(
           page - 1,
           pageSize,
-          statusList.split(',').map(status => InvoiceStatus[status])
+          `${statusList || InvoiceStatus['DRAFT']}`.split(',').map(status => InvoiceStatus[status])
         )
       );
     });
