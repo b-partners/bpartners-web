6d9a3bfd85aec01a13998ce07138146e4b742597
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-07-08 09:23:24 +0300

    chore: config vite env variable import

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  // depending on your application, base can also be "/"
-  base: '',
-  plugins: [react(), viteTsconfigPaths()],
-  server: {
-    // this ensures that the browser opens upon server start
-    open: true,
-    // this sets a default port to 3000
-    port: 3000,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env': env,
+    },
+    plugins: [react(), viteTsconfigPaths()],
+    server: {
+      open: true,
+      port: 3000,
+    },
+  };
 });
