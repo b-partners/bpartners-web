d17ec20c4615053e3793b2fcda6ad55dba65b5d7
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-07-09 07:48:53 +0300

    chore: update test start command

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   video: false,
@@ -11,27 +11,24 @@ export default defineConfig({
   viewportWidth: 2014,
   viewportHeight: 844,
   defaultCommandTimeout: 30000,
-  reporter: "cypress-multi-reporters",
+  reporter: 'cypress-multi-reporters',
 
   reporterOptions: {
-    reporterEnabled: "cypress-sonarqube-reporter",
-    mergeFileName: "test-reports.xml",
+    reporterEnabled: 'cypress-sonarqube-reporter',
+    mergeFileName: 'test-reports.xml',
     cypressSonarqubeReporterReporterOptions: {
       overwrite: true,
     },
   },
 
-  projectId: "4f6tz2",
+  projectId: '4f6tz2',
 
   component: {
     setupNodeEvents(on, config) {},
-    specPattern: "src/**/*.cy.{js,ts,jsx,tsx}",
-  },
-
-  component: {
+    specPattern: 'src/**/*.cy.{js,ts,jsx,tsx}',
     devServer: {
-      framework: "react",
-      bundler: "vite",
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 });
