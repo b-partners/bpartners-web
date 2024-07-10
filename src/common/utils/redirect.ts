0763fd6448a979dd34bc91adef56777eeade653c
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-07-10 11:24:02 +0300

    chore: update redirect function

--- src/common/utils/redirect.ts
+++ src/common/utils/redirect.ts
@@ -1,8 +1,14 @@
-export const redirect =
+export class Redirect {
   // note(test-redirect): window.location.replace does not work with cypress.
   // Hence we need to wrap it here so that we can mock it when testing, as per https://glebbahmutov.com/blog/stub-react-import
   // Cypress maintainer recommends another way, but it's too elaborate: https://www.youtube.com/watch?v=mML36U62-fQ
-  (newLocation: string): void => window.location.replace(newLocation);
-
-export const mailTo = (email: string) => redirect(`mailto:${email}`);
-export const phoneTo = (phone: string) => redirect(`tel:${phone}`);
+  public static toURL(newLocation: string) {
+    return window.location.replace(newLocation);
+  }
+  public static toMail(email: string) {
+    return this.toURL(`mailto:${email}`);
+  }
+  public static toPone(phone: string) {
+    return this.toURL(`tel:${phone}`);
+  }
+}
