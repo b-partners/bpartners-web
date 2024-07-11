d94a64792c2e77445599955b3727ab84eab43a83
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-07-11 16:37:28 +0300

    chore(Login): resolve sonar lint

--- src/__tests__/Login.cy.jsx
+++ src/__tests__/Login.cy.jsx
@@ -44,7 +44,6 @@ describe(specTitle('Login'), () => {
   });
 
   it.skip('MainPage redirects to onboarding url', () => {
-    // TODO(cognito-signup)
     cy.mount(<App />);
     cy.intercept('POST', '/onboardingInitiation', { redirectionUrl: 'https://authUrl.com' }).as('onboardingInitiation');
     cy.get('#register').contains(`Pas de compte ? C'est par ici`);
