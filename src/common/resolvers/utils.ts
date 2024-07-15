6112e496cdbe08efc4f6b42606d813138bbb7ee4
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-07-15 11:12:50 +0300

    refactor(utils): phone validator regex

--- src/common/resolvers/utils.ts
+++ src/common/resolvers/utils.ts
@@ -34,7 +34,7 @@ export const requiredNumberRows = () =>
     .transform(value => parseInt(value, 10))
     .refine(value => value >= 0, { message: FieldErrorMessage.minNumberNotValid });
 
-export const phoneValidator = (phoneNumber: string) => /^[0-9\s/+/-]*$/.test(phoneNumber);
+export const phoneValidator = (phoneNumber: string) => /^[0-9\s+/-]*$/.test(phoneNumber);
 
 export const requiredArray = () => z.string({ required_error: FieldErrorMessage.required }).array().nonempty({ message: FieldErrorMessage.required });
 export const emailValidator = z.string().email({ message: FieldErrorMessage.emailNotValid });
