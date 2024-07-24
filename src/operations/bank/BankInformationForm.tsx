d4a6a252707e28ab0b8c6ee6c05089c03485146e
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-07-24 11:24:00 +0300

    refactor: BankInformationForm to tsx

--- src/operations/bank/BankInformationForm.tsx
+++ src/operations/bank/BankInformationForm.tsx
@@ -1,20 +1,26 @@
-import { Save as SaveIcon } from '@mui/icons-material';
-import { Box, Button, Paper, Typography } from '@mui/material';
-import { useState } from 'react';
-import { useNotify, useRefresh } from 'react-admin';
-import { FormProvider, useForm } from 'react-hook-form';
 import { BpFormField } from '@/common/components';
 import { handleSubmit } from '@/common/utils';
 import { accountProvider } from '@/providers';
+import { Account } from '@bpartners/typescript-client';
+import { Save as SaveIcon } from '@mui/icons-material';
+import { Box, Button, Paper, Typography } from '@mui/material';
+import { FC, useState } from 'react';
+import { useNotify, useRefresh } from 'react-admin';
+import { FormProvider, useForm } from 'react-hook-form';
 import { BIC_MESSAGE_CONTAINER } from './style';
 
-export const BankInformationForm = props => {
+interface BankInformationFormProps {
+  account: Account;
+  setAccount: (account: Account) => void;
+}
+
+export const BankInformationForm: FC<BankInformationFormProps> = props => {
   const {
     account: { name, bic, iban },
     setAccount,
   } = props;
   const form = useForm({ mode: 'all', defaultValues: { name, bic, iban } });
-  const [isLoading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const notify = useNotify();
   const refresh = useRefresh();
 
@@ -24,16 +30,16 @@ export const BankInformationForm = props => {
       refresh();
       setAccount(newAccount);
     };
-    setLoading(true);
+    setIsLoading(true);
     fetch()
       .catch(() => notify('messages.global.error', { type: 'error' }))
-      .finally(() => setLoading(false));
+      .finally(() => setIsLoading(false));
   });
 
   return (
     <Box>
       <Paper sx={BIC_MESSAGE_CONTAINER}>
-        <Typography variant='p' component='div' sx={{ margin: 2 }}>
+        <Typography variant='body1' component='div' sx={{ margin: 2 }}>
           Pour finaliser votre synchronisation de compte bancaire, veuillez renseigner votre BIC présent sur votre RIB et enregistrer.
         </Typography>
       </Paper>
