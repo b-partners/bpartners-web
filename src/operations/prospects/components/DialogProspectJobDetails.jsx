8a25859dfa9e08c9d7ac0033b32f5eb10f9d0a98
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-08-13 16:14:09 +0300

    fix: prospect creation input spacing

--- src/operations/prospects/components/DialogProspectJobDetails.jsx
+++ src/operations/prospects/components/DialogProspectJobDetails.jsx
@@ -1,50 +1,48 @@
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
 import { useProspectContext } from '@/common/store/prospect-store';
 import { prospectingJobsProvider } from '@/providers';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { parseRatingLastEvaluation } from '../utils';
 
 export const DialogProspectJobDetails = () => {
   const { prospectJobDetails, isOpenPopup, toggleJobDetailsPopup } = useProspectContext();
   const [prospectsLength, setProspectsLength] = useState(0);
-  const [prospectsLengthLoading, setprospectsLengthLoading] = useState(true);
+  const [prospectsLengthLoading, setProspectsLengthLoading] = useState(true);
 
   useEffect(() => {
     const getProspectEvaluationJobDetails = async () => {
       const response = await prospectingJobsProvider.getOne(prospectJobDetails?.id);
       setProspectsLength(response?.results.length);
-      setprospectsLengthLoading(false);
+      setProspectsLengthLoading(false);
     };
     getProspectEvaluationJobDetails();
   }, [prospectJobDetails]);
 
   return (
-    <>
-      <Dialog open={isOpenPopup} onClose={toggleJobDetailsPopup}>
-        <DialogTitle>Détails</DialogTitle>
-        <DialogContent>
-          <Box>
-            {!prospectsLengthLoading && prospectJobDetailsList('Nombre de prospects évalués', prospectsLength)}
-            {prospectJobDetailsList(`Types d'intervention`, prospectJobDetails?.metadata?.interventionTypes?.replace(/,/g, ', '))}
-            {prospectJobDetailsList('Date', parseRatingLastEvaluation(prospectJobDetails?.startedAt))}
-            {prospectJobDetailsList("Type d'infestation", prospectJobDetails?.metadata?.infestationType)}
-            {prospectJobDetailsList('Profession', prospectJobDetails?.metadata?.profession)}
-            {prospectJobDetailsList('SheetName', prospectJobDetails?.metadata?.sheetName)}
-            {prospectJobDetailsList('spreadsheetName', prospectJobDetails?.metadata?.spreadsheetName)}
-            {prospectJobDetailsList('Nombre min de lignes', prospectJobDetails?.metadata?.min)}
-            {prospectJobDetailsList('Nombre max de lignes', prospectJobDetails?.metadata?.max)}
-            {prospectJobDetailsList('Note min du client', prospectJobDetails?.metadata?.minCustomerRating)}
-            {prospectJobDetailsList('Note min des prospects', prospectJobDetails?.metadata?.minProspectRating)}
-            {prospectJobDetailsList('Message', prospectJobDetails?.status?.message)}
-          </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button data-testid='close-dialog-prospectJob-id' onClick={toggleJobDetailsPopup}>
-            Fermer
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+    <Dialog open={isOpenPopup} onClose={toggleJobDetailsPopup}>
+      <DialogTitle>Détails</DialogTitle>
+      <DialogContent>
+        <Box>
+          {!prospectsLengthLoading && prospectJobDetailsList('Nombre de prospects évalués', prospectsLength)}
+          {prospectJobDetailsList(`Types d'intervention`, prospectJobDetails?.metadata?.interventionTypes?.replace(/,/g, ', '))}
+          {prospectJobDetailsList('Date', parseRatingLastEvaluation(prospectJobDetails?.startedAt))}
+          {prospectJobDetailsList("Type d'infestation", prospectJobDetails?.metadata?.infestationType)}
+          {prospectJobDetailsList('Profession', prospectJobDetails?.metadata?.profession)}
+          {prospectJobDetailsList('SheetName', prospectJobDetails?.metadata?.sheetName)}
+          {prospectJobDetailsList('spreadsheetName', prospectJobDetails?.metadata?.spreadsheetName)}
+          {prospectJobDetailsList('Nombre min de lignes', prospectJobDetails?.metadata?.min)}
+          {prospectJobDetailsList('Nombre max de lignes', prospectJobDetails?.metadata?.max)}
+          {prospectJobDetailsList('Note min du client', prospectJobDetails?.metadata?.minCustomerRating)}
+          {prospectJobDetailsList('Note min des prospects', prospectJobDetails?.metadata?.minProspectRating)}
+          {prospectJobDetailsList('Message', prospectJobDetails?.status?.message)}
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button data-testid='close-dialog-prospectJob-id' onClick={toggleJobDetailsPopup}>
+          Fermer
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
