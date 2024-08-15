6db93d467f6d10bada44b88e7f3d1ae4da2d4594
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-08-15 12:46:31 +0300

    fix: types on to-array-buffer

--- src/common/utils/to-array-buffer.ts
+++ src/common/utils/to-array-buffer.ts
@@ -1,6 +1,6 @@
 import { ChangeEvent } from 'react';
 
-export const toArrayBuffer = (e: ChangeEvent<HTMLInputElement>) =>
+export const toArrayBuffer = (e: ChangeEvent<HTMLInputElement>): Promise<any> =>
   new Promise((resolve, reject) => {
     let file = e.target.files[0];
     const reader = new FileReader();
