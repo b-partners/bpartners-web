c74e1fba645e14235e07a975493a93408e92c1dd
RickaPrincy <rckprincy@gmail.com>
2024-09-10 08:32:29 +0300

    chore: use get by id to retrieve prospect value

--- src/operations/prospects/DraftAreaPictureAnnotations.tsx
+++ src/operations/prospects/DraftAreaPictureAnnotations.tsx
@@ -1,10 +1,9 @@
 import ListComponent from '@/common/components/ListComponent';
 import Pagination from '@/common/components/Pagination';
-import { DraftAreaPictureAnnotation, Prospect } from '@bpartners/typescript-client';
-import { Box, LinearProgress, SxProps } from '@mui/material';
-import { useMemo } from 'react';
-import { Empty, List, useGetList, useListContext } from 'react-admin';
-import { DraftAnnotationItem, DraftAnnotationItemProps } from './components';
+import { DraftAreaPictureAnnotation } from '@bpartners/typescript-client';
+import { Box, SxProps } from '@mui/material';
+import { Empty, List, useListContext } from 'react-admin';
+import { DraftAnnotationItem } from './components';
 
 const DRAFT_ANNOTATION_ITEM_WRAPPER_SX: SxProps = {
   width: '100%',
@@ -15,12 +14,6 @@ const DRAFT_ANNOTATION_ITEM_WRAPPER_SX: SxProps = {
 };
 
 export const DraftAreaPictureAnnotations = () => {
-  const { data: prospets, isLoading: isFetchingProspects } = useGetList('prospects');
-
-  if (isFetchingProspects) {
-    return <LinearProgress color='primary' />;
-  }
-
   return (
     <List
       component={ListComponent}
@@ -29,27 +22,17 @@ export const DraftAreaPictureAnnotations = () => {
       resource='drafts-annotations'
       pagination={<Pagination />}
     >
-      <DraftAreaPictureAnnotationContent prospets={prospets} />
+      <DraftAreaPictureAnnotationContent />
     </List>
   );
 };
 
-const DraftAreaPictureAnnotationContent = ({ prospets }: { prospets: Prospect[] }) => {
-  const { data: draftsAnnotations = [], page, perPage } = useListContext<Required<DraftAreaPictureAnnotation>>();
-
-  const mappedDraftAnnotations: DraftAnnotationItemProps['mappedAnnotation'][] = useMemo(() => {
-    return draftsAnnotations.map(annotation => {
-      return {
-        draftAnnotation: annotation,
-        prospect: prospets.find(prospect => prospect.id === annotation.areaPicture.prospectId),
-      };
-    });
-  }, [prospets, draftsAnnotations, page, perPage]);
-
+const DraftAreaPictureAnnotationContent = () => {
+  const { data: draftsAnnotations = [] } = useListContext<Required<DraftAreaPictureAnnotation>>();
   return (
     <Box sx={DRAFT_ANNOTATION_ITEM_WRAPPER_SX}>
-      {mappedDraftAnnotations.map(mappedAnnotation => {
-        return <DraftAnnotationItem key={mappedAnnotation.draftAnnotation.id} mappedAnnotation={mappedAnnotation} />;
+      {draftsAnnotations.map(draftAnnotation => {
+        return <DraftAnnotationItem key={draftAnnotation.id} draftAnnotation={draftAnnotation} />;
       })}
     </Box>
   );
--- src/operations/prospects/components/DraftAnnotationItem.tsx
+++ src/operations/prospects/components/DraftAnnotationItem.tsx
@@ -3,7 +3,8 @@ import { formatDateTime, getFileUrl } from '@/common/utils';
 import { DraftAreaPictureAnnotation, FileType, Prospect, ZoomLevel } from '@bpartners/typescript-client';
 import { Comment, Home, LocalPhoneOutlined, MailOutline, Star, Update } from '@mui/icons-material';
 import { Box, Paper, SxProps, Typography } from '@mui/material';
-import { FC } from 'react';
+import { FC, useCallback } from 'react';
+import { useGetOne } from 'react-admin';
 import { useNavigate } from 'react-router';
 import { parseRatingLastEvaluation, parseRatingValue } from '../utils';
 import { CardViewField } from './CardViewField';
@@ -16,19 +17,12 @@ const DRAFT_ANNOTATION_ITEM_SX: SxProps = {
 };
 
 export type DraftAnnotationItemProps = {
-  mappedAnnotation?: {
-    draftAnnotation: DraftAreaPictureAnnotation;
-    prospect?: Prospect;
-  };
+  draftAnnotation: DraftAreaPictureAnnotation;
 };
 
-export const DraftAnnotationItem: FC<DraftAnnotationItemProps> = ({ mappedAnnotation }) => {
-  const { draftAnnotation, prospect } = mappedAnnotation;
+export const DraftAnnotationItem: FC<DraftAnnotationItemProps> = ({ draftAnnotation }) => {
   const navigate = useNavigate();
-
-  if (!prospect) {
-    return null;
-  }
+  const { data: prospect = {} as Prospect, isLoading } = useGetOne<Required<Prospect>>('prospects', { id: draftAnnotation.areaPicture?.id });
 
   const navigateToAnnotation = () => {
     const { fileId, prospectId, id: pictureId } = draftAnnotation.areaPicture;
@@ -38,24 +32,31 @@ export const DraftAnnotationItem: FC<DraftAnnotationItemProps> = ({ mappedAnnota
     );
   };
 
+  const getLoadingValue = useCallback(
+    (value: string) => {
+      return isLoading ? 'Chargement...' : value;
+    },
+    [isLoading]
+  );
+
+  const lastEvaluation =
+    prospect?.rating?.lastEvaluation === null ? 'Non renseigné' : parseRatingLastEvaluation(prospect?.rating?.lastEvaluation.toISOString());
+
   return (
     <Paper sx={DRAFT_ANNOTATION_ITEM_SX} component='div'>
       <Typography variant='subtitle1' sx={{ textTransform: 'uppercase' }}>
-        {prospect.name || 'Non renseigné'}
+        {getLoadingValue(prospect.name || 'Non renseigné')}
       </Typography>
       <Typography variant='subtitle1' sx={{ fontSize: '14px' }}>
-        Brouillon modifié le: {formatDateTime(new Date(draftAnnotation.creationDatetime))}
+        {getLoadingValue(`Brouillon modifié le: ${formatDateTime(new Date(draftAnnotation.creationDatetime))}`)}
       </Typography>
       <Box sx={{ color: '#4d4d4d', mb: 1 }}>
-        <CardViewField icon={<MailOutline />} value={prospect.email} />
-        <CardViewField icon={<LocalPhoneOutlined />} value={prospect.phone} />
-        <CardViewField icon={<Home />} value={prospect.address} />
-        <CardViewField icon={<Comment />} value={prospect.comment ? prospect.comment : prospect.defaultComment} />
-        <CardViewField icon={<Star />} value={parseRatingValue(prospect?.rating?.value)} />
-        <CardViewField
-          icon={<Update />}
-          value={prospect?.rating?.lastEvaluation === null ? 'Non renseigné' : parseRatingLastEvaluation(prospect?.rating?.lastEvaluation.toISOString())}
-        />
+        <CardViewField icon={<MailOutline />} value={getLoadingValue(prospect.email)} />
+        <CardViewField icon={<LocalPhoneOutlined />} value={getLoadingValue(prospect.phone)} />
+        <CardViewField icon={<Home />} value={getLoadingValue(prospect.address)} />
+        <CardViewField icon={<Comment />} value={getLoadingValue(prospect.comment ? prospect.comment : prospect.defaultComment)} />
+        <CardViewField icon={<Star />} value={getLoadingValue(parseRatingValue(prospect?.rating?.value))} />
+        <CardViewField icon={<Update />} value={getLoadingValue(lastEvaluation)} />
       </Box>
       <BPButton label='resources.draftsAnnotations.finish' onClick={navigateToAnnotation} />
     </Paper>
