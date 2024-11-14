c09ccd169333a6360192404610dc5cc5980b0685
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2024-11-14 11:05:29 +0300

    build: add husky for lint

--- src/providers/customer-provider.ts
+++ src/providers/customer-provider.ts
@@ -20,7 +20,7 @@ const rmRaProps = (dirtyCustomer: any): Customer => {
 };
 
 export const customerProvider: BpDataProviderType = {
-  getList: async function(page = 1, perPage = maxPageSize, filters = {}): Promise<any[]> {
+  getList: async function (page = 1, perPage = maxPageSize, filters = {}): Promise<any[]> {
     const { customerListSearch } = filters;
     const searchValues = ((customerListSearch as string) || '').split(' ');
     const { accountId } = await asyncGetUserInfo();
@@ -40,15 +40,15 @@ export const customerProvider: BpDataProviderType = {
     );
     return data;
   },
-  getOne: async function(customerId: string): Promise<any> {
+  getOne: async function (customerId: string): Promise<any> {
     const { accountId } = getCached.userInfo();
     return (await customerApi().getCustomerById(accountId, customerId)).data;
   },
-  saveOrUpdate: async function([resource]: any[]): Promise<any[]> {
+  saveOrUpdate: async function ([resource]: any[]): Promise<any[]> {
     const { accountId } = getCached.userInfo();
     return (await customerApi().createCustomers(accountId, [rmRaProps(resource)])).data;
   },
-  update: async function([resource]: any[]): Promise<any[]> {
+  update: async function ([resource]: any[]): Promise<any[]> {
     const { accountId } = getCached.userInfo();
     return (await customerApi().updateCustomers(accountId, [rmRaProps(resource)])).data;
   },
