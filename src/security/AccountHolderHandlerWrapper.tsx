4ea458106341d78e62f8520fc72f9ef988178bb4
RickaPrincy <rckprincy@gmail.com>
2025-01-29 20:43:52 +0300

    chore: avoid retry on forbidden error in account holder wrapper

--- src/security/AccountHolderHandlerWrapper.tsx
+++ src/security/AccountHolderHandlerWrapper.tsx
@@ -1,15 +1,22 @@
 import { BPLoader } from '@/common/components';
 import { accountHolderProvider, authProvider } from '@/providers';
 import { useQuery } from '@tanstack/react-query';
+import { isAxiosError } from 'axios';
 import { FC, PropsWithChildren } from 'react';
 
 export const AccountHolderHandlerWrapper: FC<PropsWithChildren> = ({ children }) => {
   const isSubscribed = authProvider.isSubscribed();
-  const { data: accountHolder = null, isLoading } = useQuery({
+  const { isLoading, error, isError } = useQuery({
     retry: 7,
     queryKey: ['accountHolder'],
     queryFn: () => (isSubscribed ? accountHolderProvider.getOne() : null),
   });
 
-  return (isLoading ?? !accountHolder) ? <BPLoader message='Chargement des données de votre compte...' /> : children;
+  if (isError && isAxiosError(error)) {
+    if (error.status === 403) {
+      return children;
+    }
+  }
+
+  return isLoading ? <BPLoader message='Chargement des données de votre compte...' /> : children;
 };
