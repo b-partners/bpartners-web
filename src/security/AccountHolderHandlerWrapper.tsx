6db142be1331f5ac3e81df3757cc66f9b0724fe5
RickaPrincy <rckprincy@gmail.com>
2025-02-01 09:15:49 +0300

    chore: handle auth error

--- src/security/AccountHolderHandlerWrapper.tsx
+++ src/security/AccountHolderHandlerWrapper.tsx
@@ -1,22 +1,26 @@
 import { BPLoader } from '@/common/components';
 import { accountHolderProvider, authProvider } from '@/providers';
 import { useQuery } from '@tanstack/react-query';
-import { isAxiosError } from 'axios';
+import { AxiosError, isAxiosError } from 'axios';
 import { FC, PropsWithChildren } from 'react';
 
+const isAuthError = (error: AxiosError) => error?.status === 401 || error?.status === 403;
 export const AccountHolderHandlerWrapper: FC<PropsWithChildren> = ({ children }) => {
   const isSubscribed = authProvider.isSubscribed();
-  const { isLoading, error, isError } = useQuery({
-    retry: 7,
+  const { isLoading } = useQuery({
+    retry: 5,
     queryKey: ['accountHolder'],
-    queryFn: () => (isSubscribed ? accountHolderProvider.getOne() : null),
+    queryFn: async () => {
+      try {
+        return isSubscribed ? await accountHolderProvider.getOne() : null;
+      } catch (error) {
+        if (isAxiosError(error) && isAuthError(error)) {
+          return null;
+        }
+        throw error;
+      }
+    },
   });
-
-  if (isError && isAxiosError(error)) {
-    if (error.status === 403) {
-      return children;
-    }
-  }
 
   return isLoading ? <BPLoader message='Chargement des données de votre compte...' /> : children;
 };
