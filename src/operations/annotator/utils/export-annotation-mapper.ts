86927b5ef7a36261aab91b86bc22dedce9aa1304
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2025-02-04 08:06:35 +0300

    feat: export annotation from api & refactor annotation show infos
    * build: bump client version
    
    * refactor: annotationInfo show
    
    * refactor: rename AnnotationInfo component to avoid duplicate name
    
    * chore: remove bp construction
    
    * feat: export area picture annotation from api
    
    * test(fix): export area picture annotation
    
    * style: fix typo

---
+++ src/operations/annotator/utils/export-annotation-mapper.ts
@@ -0,0 +1,40 @@
+import { emptyToNull } from '@/common/utils';
+import { Measurement, Polygon } from '@bpartners/annotator-component';
+import { ExportAreaPictureAnnotation, ExportAreaPictureAnnotationMeasurement } from '@bpartners/typescript-client';
+import { AnnotationInfo } from '../types';
+import { createDefaultAnnotationInfo } from './annotation-info-mapper';
+import { translateAnnotationInfo } from './annotation-info-translator';
+
+export type ExportAnnotationMapperArgs = {
+  imageUrl: string;
+  address: string;
+  polygons: Polygon[];
+  annotationInfos: AnnotationInfo[];
+};
+
+export const exportAnnotationMapper = ({ annotationInfos, imageUrl, address, polygons }: ExportAnnotationMapperArgs): ExportAreaPictureAnnotation => {
+  return {
+    imageUrl,
+    address,
+    annotations: polygons.map((polygon, index) => {
+      const annotationInfo = annotationInfos.find(info => info.polygonId === polygon.id) ?? createDefaultAnnotationInfo(polygon.id, index);
+
+      return {
+        polygon,
+        labelName: annotationInfo?.labelName,
+        fillColor: polygon?.fillColor,
+        strokeColor: polygon?.strokeColor,
+        measurements: polygon.measurements?.slice(1).map(exportMeasurementMapper),
+        infos: translateAnnotationInfo({ ...emptyToNull(annotationInfo), area: polygon.surface }),
+      };
+    }),
+  };
+};
+
+const exportMeasurementMapper = (measurment: Measurement): ExportAreaPictureAnnotationMeasurement => {
+  return {
+    unit: measurment.unity,
+    value: parseFloat(measurment.value.toFixed(2)),
+    isInvisible: measurment.isInvisible,
+  };
+};
