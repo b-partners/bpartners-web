c4e8a7a46872d33a5a591569ebeb2b57c6602002
RickaPrincy <rckprincy@gmail.com>
2025-03-13 22:00:25 +0300

    style: draft annotation

--- src/operations/prospects/components/DraftAnnotationItem.tsx
+++ src/operations/prospects/components/DraftAnnotationItem.tsx
@@ -2,11 +2,12 @@ import { FC, useCallback } from 'react';
 import { useGetOne } from 'react-admin';
 import { useNavigate } from 'react-router';
 
-import { BPButton } from '@/common/components';
+import { BP_COLOR } from '@/bp-theme';
+import { BPButton, FlexBox } from '@/common/components';
 import { formatDateTime, getFileUrl } from '@/common/utils';
 import { clearPolygons } from '@/providers';
 import { DraftAreaPictureAnnotation, FileType, Prospect, ZoomLevel } from '@bpartners/typescript-client';
-import { Comment, Home, LocalPhoneOutlined, MailOutline, Star, Update } from '@mui/icons-material';
+import { CheckCircleOutlined, Comment, LocalPhoneOutlined, LocationOnOutlined, MailOutline, Star, Update } from '@mui/icons-material';
 import { Box, Paper, SxProps, Typography } from '@mui/material';
 import { parseRatingValue } from '../utils';
 import { CardViewField } from './CardViewField';
@@ -17,7 +18,8 @@ const DRAFT_ANNOTATION_ITEM_SX: SxProps = {
     lg: '400px',
     sm: '100%',
   },
-  p: 1,
+  px: 1,
+  pb: 1,
   gap: 2,
 };
 
@@ -59,26 +61,37 @@ export const DraftAnnotationItem: FC<DraftAnnotationItemProps> = ({ draftAnnotat
 
   return (
     <Paper data-cy='draft-item' sx={DRAFT_ANNOTATION_ITEM_SX} component='div'>
-      <Typography variant='subtitle1' sx={{ textTransform: 'uppercase' }}>
-        {getLoadingValue(prospect.name || 'Non renseigné')}
-      </Typography>
-      <Typography variant='subtitle1' sx={{ fontSize: '14px' }}>
-        {getLoadingValue(`Brouillon modifié le: ${formatDateTime(new Date(draftAnnotation.creationDatetime))}`)}
-      </Typography>
-      <Box sx={{ color: '#4d4d4d', mb: 1 }}>
-        <CardViewField icon={<MailOutline />} value={getLoadingValue(prospect.email)} />
-        <CardViewField icon={<LocalPhoneOutlined />} value={getLoadingValue(prospect.phone)} />
-        <CardViewField icon={<Home />} value={getLoadingValue(prospect.address)} />
-        <CardViewField icon={<Comment />} value={getLoadingValue(prospect.comment ? prospect.comment : prospect.defaultComment)} />
-        <CardViewField icon={<Star />} value={getLoadingValue(parseRatingValue(prospect?.rating?.value))} />
-        <CardViewField icon={<Update />} value={getLastEvaluationValue()} />
+      <Box sx={{ py: 2, px: 1 }}>
+        <Typography variant='subtitle1' sx={{ textTransform: 'uppercase', fontWeight: 'bold' }}>
+          {getLoadingValue(prospect.name || 'Non renseigné')}
+        </Typography>
+        <Typography variant='subtitle1' sx={{ fontSize: '14px' }}>
+          {getLoadingValue(`Brouillon modifié le: ${formatDateTime(new Date(draftAnnotation.creationDatetime))}`)}
+        </Typography>
+        <FlexBox
+          sx={{
+            gap: '5px',
+            color: '#4d4d4d',
+            flexDirection: 'column',
+            alignItems: 'start',
+            justifyContent: 'start',
+          }}
+        >
+          <CardViewField icon={<LocationOnOutlined color='primary' />} value={getLoadingValue(prospect.address)} />
+          <CardViewField icon={<MailOutline color='success' />} value={getLoadingValue(prospect.email)} />
+          <CardViewField icon={<LocalPhoneOutlined color='warning' />} value={getLoadingValue(prospect.phone)} />
+          <CardViewField icon={<Comment />} value={getLoadingValue(prospect.comment ? prospect.comment : prospect.defaultComment)} />
+          <CardViewField icon={<Star />} value={getLoadingValue(parseRatingValue(prospect?.rating?.value))} />
+          <CardViewField icon={<Update />} value={getLastEvaluationValue()} />
+        </FlexBox>
       </Box>
       <BPButton
         disabled={isLoading}
         data-cy='finish-draft-btn'
         label='resources.draftsAnnotations.finish'
         onClick={navigateToAnnotation}
-        sx={{ width: '98% !important', display: 'block' }}
+        icon={<CheckCircleOutlined />}
+        sx={{ width: '95% !important', bgcolor: BP_COLOR['5'] }}
       />
     </Paper>
   );
