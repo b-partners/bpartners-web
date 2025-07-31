1ff7db1a84eb431b7e1dee11e93094a573e5c3d0
Swonath <lanathaud@gmail.com>
2025-07-31 16:53:38 +0200

    chore/create block home page

--- src/operations/home/Home.tsx
+++ src/operations/home/Home.tsx
@@ -1,180 +1,253 @@
-import { BP_COLOR } from '@/bp-theme';
-import { BPButton, FlexBox } from '@/common/components';
-import { useToggle } from '@/common/hooks';
-import { handleSubmit } from '@/common/utils';
-import { Box, BoxProps, Divider, Typography, useMediaQuery } from '@mui/material';
-import { BaseSyntheticEvent, FC } from 'react';
-import { ProspectDialog } from '../prospects/components';
-import { ProspectDialogProvider } from '../prospects/ProspectsList';
+import { BPButton } from '@/common/components';
+import { PALETTE_COLORS } from '@/common/config/theme';
+import { Add } from '@mui/icons-material';
+import PublicIcon from '@mui/icons-material/Public';
+import { Box, Divider, Grid, IconButton, TextField, Typography } from '@mui/material';
+import { useRecordContext } from 'react-admin';
 
-type GetResponsiveValueType = <T extends string | number>(sm: T, lg: T, xl?: T) => T;
-const HomeAnnotationInfo: FC<{
-  label: string;
-  value: string;
-  getResponsiveValue: GetResponsiveValueType;
-}> = ({ label, value, getResponsiveValue }) => {
-  const isCustomizedSmall = useMediaQuery('(max-width:1300px)');
-
-  return (
-    <Box
-      sx={{
-        px: 2,
-        py: getResponsiveValue(1, 1.2, 2.5),
-        my: getResponsiveValue(1, 1.5, 3),
-        width: isCustomizedSmall ? '220px' : getResponsiveValue('250px', '350px', '400px'),
-        fontSize: isCustomizedSmall ? '.6rem' : getResponsiveValue('.7rem', '1rem', '1.2rem'),
-        bgcolor: BP_COLOR['solid_grey'],
-      }}
-    >
-      <Typography sx={{ fontWeight: 'bold', fontSize: 'inherit' }}>{label}</Typography>
-      <Typography sx={{ fontSize: 'inherit' }}>{value}</Typography>
-    </Box>
-  );
-};
-
-const ShadowedBoxImage: FC<BoxProps & { size: string | number; bgurl: string }> = ({ bgurl, sx = {}, size, children, ...boxProps }) => {
-  return (
-    <Box sx={{ width: size, height: size, backgroundImage: `url(${bgurl})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      <Box sx={{ bgcolor: 'rgba(0,0,0,.2)', width: '100%', height: '100%', ...sx }} {...boxProps}>
-        {children}
-      </Box>
-    </Box>
-  );
-};
+const prospectsList = [
+  { name: 'Jean Dupont', address: '14 avenue d’Italie, Paris 75020' },
+  { name: 'Jean Dupont', address: '14 avenue d’Italie, Paris 75020' },
+  { name: 'Jean Dupont', address: '14 avenue d’Italie, Paris 75020' },
+  { name: 'Jean Dupont', address: '14 avenue d’Italie, Paris 75020' },
+  { name: 'Jean Dupont', address: '14 avenue d’Italie, Paris 75020' },
+  { name: 'Jean Dupont', address: '14 avenue d’Italie, Paris 75020' },
+];
 
 export const Home = () => {
-  const isCustomizedLarge = useMediaQuery('(min-width:1800px)');
-  const isCustomizedExtraLarge = useMediaQuery('(min-width:2500px)');
-  const isCustomizedSmall = useMediaQuery('(max-width:1300px)');
-
-  const getResponsiveValue: GetResponsiveValueType = (sm, lg, xl) => {
-    if (isCustomizedExtraLarge) {
-      return xl ?? lg;
-    }
-    if (isCustomizedLarge) {
-      return lg;
-    }
-    return sm;
-  };
-
+  const record = useRecordContext();
+  console.log(record);
   return (
-    <main>
-      <Typography
-        sx={{
-          mx: 'auto',
-          fontWeight: 'bold',
-          textAlign: 'center',
-          mb: getResponsiveValue(1, 2, 3),
-          mt: getResponsiveValue(1, 2, 8),
-          fontSize: getResponsiveValue('1.4rem', '2rem', '2.8rem'),
-          maxWidth: getResponsiveValue('700px', '1000px', '1500px'),
-        }}
-      >
-        Pour demarrer ajoutez une adresse et commencez à <span style={{ color: BP_COLOR['5'] }}>analyser les toitures de vos clients et prospects</span>
+    <Box>
+      <Typography sx={{ fontSize: '2rem', textAlign: 'center', color: PALETTE_COLORS.neon_orange, fontWeight: 'bold', bgcolor: PALETTE_COLORS.white }}>
+        Bienvenue sur le dashboard de Birdia
       </Typography>
-      <FlexBox sx={{ gap: getResponsiveValue(3, 5, 6), alignItems: 'start' }}>
-        <Box>
-          <FlexBox>
-            <Box sx={{ transform: getResponsiveValue('translateX(30px)', 'translateX(50px)') }}>
-              <ShadowedBoxImage bgurl='/home/1.png' size={getResponsiveValue('150px', '220px', '350px')} />
-              <ShadowedBoxImage bgurl='/home/2.png' size={getResponsiveValue('150px', '220px', '350px')} />
-            </Box>
-            <ShadowedBoxImage
-              bgurl='/home/3.png'
-              size={isCustomizedSmall ? '350px' : getResponsiveValue('400px', '500px', '800px')}
+      <Box p={3} bgcolor={PALETTE_COLORS.white} minHeight='100vh'>
+        {/* ✅ Image bien cadrée et limitée */}
+        <Box
+          sx={{
+            width: '100%',
+            maxHeight: '450px',
+            minHeight: '450px',
+            overflow: 'hidden',
+            borderRadius: 3,
+            mb: 2,
+          }}
+        >
+          <img
+            src='/Account/Photo-Home-Page.jpg'
+            alt='Logo'
+            style={{
+              width: '100%',
+              height: '500px',
+              objectFit: 'cover',
+              display: 'block',
+            }}
+          />
+        </Box>
+
+        {/* Adresse en haut */}
+        <Box sx={{ display: 'flex' }}>
+          <TextField
+            fullWidth
+            variant='outlined'
+            label={
+              <>
+                <PublicIcon sx={{ verticalAlign: 'middle', mr: 1 }} />
+                adresse :
+              </>
+            }
+            sx={{
+              mb: 2,
+              '& .MuiOutlinedInput-root': {
+                borderRadius: 3,
+                '& fieldset': {
+                  borderColor: PALETTE_COLORS.black, // couleur par défaut
+                },
+                '&:hover fieldset': {
+                  borderColor: PALETTE_COLORS.neon_orange, // couleur au survol
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: PALETTE_COLORS.neon_orange, // couleur quand le champ est sélectionné
+                },
+              },
+            }}
+          />
+          <BPButton
+            sx={{ fontSize: '1rem', color: PALETTE_COLORS.white, height: '40px', borderRadius: 3, ml: 1, textAlign: 'center', mt: -0.1 }}
+            onClick={() => (window.location.pathname = '/prospects')}
+            label="Passer à l'analyse"
+          />
+        </Box>
+
+        {/* Grille principale */}
+        <Grid container spacing={3}>
+          {/* Bloc gauche avec .map() */}
+          <Grid item xs={12} md={6}>
+            <Box
               sx={{
-                p: 1,
+                boxShadow: '0 2px 6px #000000',
                 display: 'flex',
-                justifyContent: 'end',
+                justifyContent: 'center',
+                alignItems: 'center',
                 flexDirection: 'column',
+                p: 5,
+                borderRadius: 2,
+                bgcolor: PALETTE_COLORS.white,
               }}
             >
-              <Typography sx={{ fontSize: getResponsiveValue('.8rem', '1rem', '1.2rem'), fontWeight: 'bold', textAlign: 'end', color: 'white' }}>
-                Source: Image HD 5cm - Mars 2024
-              </Typography>
-              <Typography
-                sx={{
-                  ml: 'auto',
-                  color: 'white',
-                  textAlign: 'center',
-                  fontWeight: 'bold',
-                  bgcolor: BP_COLOR['5'],
-                  px: getResponsiveValue(4, 6),
-                  py: getResponsiveValue('7px', '10px'),
-                  fontSize: getResponsiveValue('.8rem', '1rem', '1.2rem'),
+              <Grid container spacing={3}>
+                {prospectsList.map((prospect, index) => (
+                  <Grid item xs={12} sm={6} key={index} minHeight={'76px'}>
+                    <Box display='flex' alignItems='center' justifyContent={'center'}>
+                      <PublicIcon sx={{ mr: 1 }} />
+                      <Box>
+                        <Typography fontWeight='bold'>{prospect.name}</Typography>
+                        <Typography variant='body2'>{prospect.address}</Typography>
+                      </Box>
+                    </Box>
+                  </Grid>
+                ))}
+              </Grid>
+            </Box>
+          </Grid>
+
+          {/* Bloc droite avec 4 blocs */}
+          <Grid item xs={12} md={6}>
+            <Grid item xs={12}>
+              <Box
+                display='flex'
+                alignItems='center'
+                justifyContent='space-between'
+                p={2}
+                mb={2}
+                borderRadius={2}
+                style={{
+                  backgroundColor: PALETTE_COLORS.white,
+                  color: PALETTE_COLORS.black,
+                  boxShadow: '0 2px 6px #000000',
                 }}
               >
-                NOTE DÉGRADATION GLOBALE: 41%
-              </Typography>
-            </ShadowedBoxImage>
-          </FlexBox>
-        </Box>
-        <Box>
-          <Typography color='gray' sx={{ pl: 1, fontWeight: 'bold', fontSize: getResponsiveValue('.8rem', '1.2rem', '1.5rem') }}>
-            ANALYSE DE TOITURE
-          </Typography>
-          <Divider sx={{ height: '2px', mb: 1 }} />
-          <FlexBox sx={{ gap: { sm: 1, xl: 4 }, alignItems: 'end' }}>
-            <Box>
-              <HomeAnnotationInfo getResponsiveValue={getResponsiveValue} label='SURFACE TOTALE' value='3 205m²' />
-              <HomeAnnotationInfo getResponsiveValue={getResponsiveValue} label='TYPE' value='PLAT' />
-              <HomeAnnotationInfo getResponsiveValue={getResponsiveValue} label='PENTE' value='0°' />
-              <HomeAnnotationInfo getResponsiveValue={getResponsiveValue} label="TAUX D'HUMIDITE" value='26%' />
-              <HomeAnnotationInfo getResponsiveValue={getResponsiveValue} label='FISSURE CASSURE' value='NÉANT' />
-              <HomeAnnotationInfo getResponsiveValue={getResponsiveValue} label='RÊVETEMENT' value='ASPHALTE GRAY' />
-            </Box>
-            <Box>
-              <HomeAnnotationInfo getResponsiveValue={getResponsiveValue} label='MUTATION HUM' value='DÉGRADATION' />
-              <HomeAnnotationInfo getResponsiveValue={getResponsiveValue} label='TAUX DE RÉPARATION' value='0%' />
-              <HomeAnnotationInfo getResponsiveValue={getResponsiveValue} label="TAUX D'USURE / MOISISSURE" value='47%' />
-              <HomeAnnotationInfo getResponsiveValue={getResponsiveValue} label='RISQUE FEU' value='NÉANT' />
-              <HomeAnnotationInfo getResponsiveValue={getResponsiveValue} label='OBSTACLE' value='7%' />
-              <HomeAnnotationInfo getResponsiveValue={getResponsiveValue} label='HÉTÉROGÉNÉITÉ DES REVÊTEMENTS' value='74%' />
-            </Box>
-          </FlexBox>
-        </Box>
-      </FlexBox>
-      <ProspectDialogProvider
-        ComponentChild={({ saveOrUpdateProspectSubmit }: Pick<CreateProspectDialogProps, 'saveOrUpdateProspectSubmit'>) => (
-          <CreateProspectDialog saveOrUpdateProspectSubmit={saveOrUpdateProspectSubmit} getResponsiveValue={getResponsiveValue} />
-        )}
-      />
-    </main>
-  );
-};
+                <IconButton style={{ color: PALETTE_COLORS.black }} onClick={() => (window.location.pathname = '/prospects')}>
+                  <Typography fontWeight='bold' fontSize='1.5rem'>
+                    Prospects
+                  </Typography>
+                </IconButton>
+                <Divider orientation='vertical' flexItem style={{ backgroundColor: PALETTE_COLORS.black, margin: '0 12px' }} />
+                <IconButton style={{ color: PALETTE_COLORS.black }} onClick={() => (window.location.pathname = '/prospects')}>
+                  <Add />
+                </IconButton>
+              </Box>
+            </Grid>
 
-type CreateProspectDialogProps = {
-  saveOrUpdateProspectSubmit: (toggleDialog: () => void, isCreating: boolean, e: BaseSyntheticEvent) => Promise<void>;
-  getResponsiveValue: GetResponsiveValueType;
-};
-const CreateProspectDialog: FC<CreateProspectDialogProps> = ({ saveOrUpdateProspectSubmit, getResponsiveValue }) => {
-  const { value: isCreating, toggleValue: toggleCreating } = useToggle();
+            <Grid container spacing={2}>
+              {/* Clients */}
+              <Grid item xs={6}>
+                <Box
+                  display='flex'
+                  alignItems='center'
+                  justifyContent='space-between'
+                  p={2}
+                  borderRadius={2}
+                  sx={{
+                    backgroundColor: PALETTE_COLORS.neon_orange,
+                    color: PALETTE_COLORS.white,
+                    boxShadow: '0 2px 6px #000000',
+                  }}
+                >
+                  <IconButton style={{ color: PALETTE_COLORS.white }} onClick={() => (window.location.href = '/customers')}>
+                    <Typography fontWeight='bold' fontSize='1.5rem'>
+                      Clients
+                    </Typography>
+                  </IconButton>
+                  <Divider orientation='vertical' flexItem style={{ backgroundColor: PALETTE_COLORS.white, margin: '0 12px' }} />
+                  <IconButton style={{ color: PALETTE_COLORS.white }} onClick={() => (window.location.pathname = '/customers/create')}>
+                    <Add />
+                  </IconButton>
+                </Box>
+              </Grid>
 
-  const saveOrUpdateProspect = (event: BaseSyntheticEvent) => saveOrUpdateProspectSubmit(toggleCreating, isCreating, event);
+              {/* Produits */}
+              <Grid item xs={6}>
+                <Box
+                  display='flex'
+                  alignItems='center'
+                  justifyContent='space-between'
+                  p={2}
+                  borderRadius={2}
+                  style={{
+                    backgroundColor: PALETTE_COLORS.pine,
+                    color: PALETTE_COLORS.white,
+                    boxShadow: '0 2px 6px #000000',
+                  }}
+                >
+                  <IconButton style={{ color: PALETTE_COLORS.white }} onClick={() => (window.location.href = '/products')}>
+                    <Typography fontWeight='bold' fontSize='1.5rem'>
+                      Produits
+                    </Typography>
+                  </IconButton>
+                  <Divider orientation='vertical' flexItem style={{ backgroundColor: PALETTE_COLORS.white, margin: '0 12px' }} />
+                  <IconButton style={{ color: PALETTE_COLORS.white }} onClick={() => (window.location.pathname = '/products/create')}>
+                    <Add />
+                  </IconButton>
+                </Box>
+              </Grid>
 
-  return (
-    <>
-      <BPButton
-        size='large'
-        onClick={toggleCreating}
-        data-testid='create-prospect-button'
-        label='resources.prospects.new'
-        sx={{
-          mx: 'auto',
-          display: 'block',
-          fontWeight: 'bold',
-          px: getResponsiveValue(2, 3),
-          py: getResponsiveValue(1.4, 2),
-          mt: getResponsiveValue(3, 4),
-          fontSize: getResponsiveValue('.9rem', '1.5rem', '2rem'),
-          width: 'fit-content !important',
-        }}
-      />
-      {isCreating && (
-        <form onSubmit={handleSubmit(saveOrUpdateProspect)} style={{ display: 'flex', flexDirection: 'column' }}>
-          <ProspectDialog open={isCreating} close={toggleCreating} saveOrUpdateProspectSubmit={saveOrUpdateProspect} isCreating={isCreating} />
-        </form>
-      )}
-    </>
+              {/* Devis */}
+              <Grid item xs={6}>
+                <Box
+                  display='flex'
+                  alignItems='center'
+                  justifyContent='space-between'
+                  p={2}
+                  borderRadius={2}
+                  style={{
+                    backgroundColor: PALETTE_COLORS.forest,
+                    color: PALETTE_COLORS.white,
+                    boxShadow: '0 2px 6px #000000',
+                  }}
+                >
+                  <IconButton style={{ color: PALETTE_COLORS.white }} onClick={() => (window.location.href = '/invoices')}>
+                    <Typography fontWeight='bold' fontSize='1.5rem'>
+                      Devis
+                    </Typography>
+                  </IconButton>
+                  <Divider orientation='vertical' flexItem style={{ backgroundColor: PALETTE_COLORS.white, margin: '0 12px' }} />
+                  <IconButton style={{ color: PALETTE_COLORS.white }} onClick={() => (window.location.pathname = '/invoices')}>
+                    <Add />
+                  </IconButton>
+                </Box>
+              </Grid>
+
+              {/* Factures */}
+              <Grid item xs={6}>
+                <Box
+                  display='flex'
+                  alignItems='center'
+                  justifyContent='space-between'
+                  p={2}
+                  borderRadius={2}
+                  style={{
+                    backgroundColor: PALETTE_COLORS.white,
+                    color: PALETTE_COLORS.black,
+                    boxShadow: '0 2px 6px #000000',
+                  }}
+                >
+                  <IconButton style={{ color: PALETTE_COLORS.black }} onClick={() => (window.location.href = '/invoices')}>
+                    <Typography fontWeight='bold' fontSize='1.5rem'>
+                      Factures
+                    </Typography>
+                  </IconButton>
+                  <Divider orientation='vertical' flexItem style={{ backgroundColor: PALETTE_COLORS.white, margin: '0 12px' }} />
+                  <IconButton style={{ color: PALETTE_COLORS.black }} onClick={() => (window.location.pathname = '/invoices')}>
+                    <Add />
+                  </IconButton>
+                </Box>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
   );
 };
