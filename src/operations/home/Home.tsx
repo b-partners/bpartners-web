a1f2896598156dba7bbf228c2206e928281bb29d
Swonath <lanathaud@gmail.com>
2025-08-01 16:29:35 +0200

    test: test home page

--- src/operations/home/Home.tsx
+++ src/operations/home/Home.tsx
@@ -38,7 +38,13 @@ export const Home = () => {
             }
             className='address-field'
           />
-          <BPButton className='btn-analyse' onClick={() => (window.location.pathname = '/prospects')} label="Passer à l'analyse" data-cy='button-analyze' data-path='/prospects'/>
+          <BPButton
+            className='btn-analyse'
+            onClick={() => (window.location.pathname = '/prospects')}
+            label="Passer à l'analyse"
+            data-cy='button-analyze'
+            data-path='/prospects'
+          />
         </Box>
         <Grid container spacing={3} maxHeight={'400px'}>
           <Grid item xs={12} md={6} height={'400px'}>
@@ -49,8 +55,12 @@ export const Home = () => {
                     <Box className='prospect-item'>
                       <PublicIcon sx={{ mr: 1 }} />
                       <Box className='prospect-text'>
-                        <Typography className='prospect-name' fontWeight='bold'>{prospect.name}</Typography>
-                        <Typography className='prospect-address' variant='body2'>{prospect.address}</Typography>
+                        <Typography className='prospect-name' fontWeight='bold'>
+                          {prospect.name}
+                        </Typography>
+                        <Typography className='prospect-address' variant='body2'>
+                          {prospect.address}
+                        </Typography>
                       </Box>
                     </Box>
                   </Grid>
@@ -61,13 +71,23 @@ export const Home = () => {
           <Grid item xs={12} md={6} minHeight={'400px'}>
             <Grid item xs={12} minHeight={'100px'}>
               <Box className='block-box block-white' mb={2}>
-                <IconButton style={{ color: PALETTE_COLORS.black }} onClick={() => (window.location.pathname = '/prospects')} data-cy='title-prospects' data-path="/prospects">
-                  <Typography className='block-title' fontWeight='bold' fontSize='1.5rem' >
+                <IconButton
+                  style={{ color: PALETTE_COLORS.black }}
+                  onClick={() => (window.location.pathname = '/prospects')}
+                  data-cy='title-prospects'
+                  data-path='/prospects'
+                >
+                  <Typography className='block-title' fontWeight='bold' fontSize='1.5rem'>
                     Prospects
                   </Typography>
                 </IconButton>
                 <Divider orientation='vertical' flexItem className='divider-black' />
-                <IconButton style={{ color: PALETTE_COLORS.black }} onClick={() => (window.location.pathname = '/prospects')} data-cy='add-prospects' data-path="/prospects">
+                <IconButton
+                  style={{ color: PALETTE_COLORS.black }}
+                  onClick={() => (window.location.pathname = '/prospects')}
+                  data-cy='add-prospects'
+                  data-path='/prospects'
+                >
                   <Add />
                 </IconButton>
               </Box>
@@ -75,52 +95,92 @@ export const Home = () => {
             <Grid container spacing={2} minHeight={'100px'}>
               <Grid item xs={6}>
                 <Box className='block-box block-orange'>
-                  <IconButton style={{ color: PALETTE_COLORS.white }} onClick={() => (window.location.href = '/customers')} data-cy='title-customers' data-path="/customers">
+                  <IconButton
+                    style={{ color: PALETTE_COLORS.white }}
+                    onClick={() => (window.location.href = '/customers')}
+                    data-cy='title-customers'
+                    data-path='/customers'
+                  >
                     <Typography className='block-title' fontWeight='bold' fontSize='1.5rem'>
                       Clients
                     </Typography>
                   </IconButton>
                   <Divider orientation='vertical' flexItem className='divider-white' />
-                  <IconButton style={{ color: PALETTE_COLORS.white }} onClick={() => (window.location.pathname = '/customers/create')} data-cy='add-customers' data-path="/customers/create">
+                  <IconButton
+                    style={{ color: PALETTE_COLORS.white }}
+                    onClick={() => (window.location.pathname = '/customers/create')}
+                    data-cy='add-customers'
+                    data-path='/customers/create'
+                  >
                     <Add />
                   </IconButton>
                 </Box>
               </Grid>
               <Grid item xs={6}>
                 <Box className='block-box block-pine'>
-                  <IconButton style={{ color: PALETTE_COLORS.white }} onClick={() => (window.location.href = '/products')} data-cy='title-products' data-path="/products">
+                  <IconButton
+                    style={{ color: PALETTE_COLORS.white }}
+                    onClick={() => (window.location.href = '/products')}
+                    data-cy='title-products'
+                    data-path='/products'
+                  >
                     <Typography className='block-title' fontWeight='bold' fontSize='1.5rem'>
                       Produits
                     </Typography>
                   </IconButton>
                   <Divider orientation='vertical' flexItem className='divider-white' />
-                  <IconButton style={{ color: PALETTE_COLORS.white }} onClick={() => (window.location.pathname = '/products/create')} data-cy='add-products' data-path="/products/create">
+                  <IconButton
+                    style={{ color: PALETTE_COLORS.white }}
+                    onClick={() => (window.location.pathname = '/products/create')}
+                    data-cy='add-products'
+                    data-path='/products/create'
+                  >
                     <Add />
                   </IconButton>
                 </Box>
               </Grid>
               <Grid item xs={6}>
                 <Box className='block-box block-forest'>
-                  <IconButton style={{ color: PALETTE_COLORS.white }} onClick={() => (window.location.href = '/invoices')} data-cy='title-invoices-1' data-path="/invoices">
+                  <IconButton
+                    style={{ color: PALETTE_COLORS.white }}
+                    onClick={() => (window.location.href = '/invoices')}
+                    data-cy='title-invoices-1'
+                    data-path='/invoices'
+                  >
                     <Typography className='block-title' fontWeight='bold' fontSize='1.5rem'>
                       Devis
                     </Typography>
                   </IconButton>
                   <Divider orientation='vertical' flexItem className='divider-white' />
-                  <IconButton style={{ color: PALETTE_COLORS.white }} onClick={() => (window.location.pathname = '/invoices?showCreateQuote=true')} data-cy='add-invoices-1' data-path="/invoices?showCreateQuote=true">
+                  <IconButton
+                    style={{ color: PALETTE_COLORS.white }}
+                    onClick={() => (window.location.pathname = '/invoices?showCreateQuote=true')}
+                    data-cy='add-invoices-1'
+                    data-path='/invoices?showCreateQuote=true'
+                  >
                     <Add />
                   </IconButton>
                 </Box>
               </Grid>
               <Grid item xs={6}>
                 <Box className='block-box block-white'>
-                  <IconButton style={{ color: PALETTE_COLORS.black }} onClick={() => (window.location.href = '/invoices')} data-cy='title-invoices-2' data-path="/invoices">
+                  <IconButton
+                    style={{ color: PALETTE_COLORS.black }}
+                    onClick={() => (window.location.href = '/invoices')}
+                    data-cy='title-invoices-2'
+                    data-path='/invoices'
+                  >
                     <Typography className='block-title' fontWeight='bold' fontSize='1.5rem'>
                       Factures
                     </Typography>
                   </IconButton>
                   <Divider orientation='vertical' flexItem className='divider-white' />
-                  <IconButton style={{ color: PALETTE_COLORS.black }} onClick={() => (window.location.pathname = '/invoices?showCreateQuote=true')} data-cy='add-invoices-2' data-path="/invoices?showCreateQuote=true">
+                  <IconButton
+                    style={{ color: PALETTE_COLORS.black }}
+                    onClick={() => (window.location.pathname = '/invoices?showCreateQuote=true')}
+                    data-cy='add-invoices-2'
+                    data-path='/invoices?showCreateQuote=true'
+                  >
                     <Add />
                   </IconButton>
                 </Box>
@@ -132,4 +192,3 @@ export const Home = () => {
     </Box>
   );
 };
-
