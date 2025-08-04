0eb84d66b3d047ec73283e2ff92c593216947b34
Swonath <lanathaud@gmail.com>
2025-08-04 15:32:39 +0200

    chore: delete excess files

--- src/operations/account/queries/index.ts
+++ src/operations/account/queries/index.ts
@@ -1,2 +1,5 @@
 export * from './business-activities-query';
 export * from './company-fields-query';
+export * from '@/operations/account/components/LogoShowLayout'; // 
+export * from '@/operations/account/components/SubjectToVatSwitch'; // 
+export * from '@/operations/account/components/types';
