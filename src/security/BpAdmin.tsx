f931b6459ff47411ab9d35004b5ca0525336d624
Swonath <lanathaud@gmail.com>
2025-08-04 16:01:22 +0200

    chore: delete excess files (pages)

--- src/security/BpAdmin.tsx
+++ src/security/BpAdmin.tsx
@@ -15,7 +15,6 @@ import { PartnersPage } from '@/operations/partners/PartnersPage';
 import products from '@/operations/products';
 import { prospects } from '@/operations/prospects';
 import transactions from '@/operations/transactions';
-import { HomePage } from '@/pages/home/utils';
 import { authProvider, awsAuth, dataProvider } from '@/providers';
 import { Admin } from '@react-admin/ra-enterprise';
 import { Resource } from '@react-admin/ra-rbac';
@@ -84,7 +83,6 @@ export const BpAdmin = () => {
         <Route path='/partners' element={<PartnersPage />} />
         <Route path='/annotator' element={<Annotator />} />
         <Route path='/error' element={<BPErrorPage />} />
-        <Route path='/homepage' element={<HomePage />} />
       </CustomRoutes>
     </Admin>
   );
