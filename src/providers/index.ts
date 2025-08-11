d96fbadcb2ee83ded04fcf064b4d7bec165cffa2
kirioka <hei.amour.2@gmail.com>
2025-08-11 10:35:11 +0300

    chore: remove transactionSupportingDoc related files

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -27,5 +27,4 @@ export * from './relaunch-provider';
 export * from './sheet-provider';
 export * from './transaction-category-provider';
 export * from './transaction-provider';
-export * from './transaction-supportingDoc-provider';
 export * from './user-subscription-provider';
