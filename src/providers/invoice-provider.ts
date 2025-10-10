b154bca7a13130116147e3c56b050d0a495868b9
kirioka <hei.amour.2@gmail.com>
2025-10-10 11:56:57 +0300

    fix: invoice list status

--- src/providers/invoice-provider.ts
+++ src/providers/invoice-provider.ts
@@ -9,8 +9,9 @@ export const invoiceProvider: BpDataProviderType = {
 
     const accountId = await asyncGetAccountId();
     const searchValues = ((invoiceListSearch as string) || '').split(' ');
+    const mappedInvoiceTypes = `${invoiceTypes.join(',')}`;
 
-    return (await payingApi().getInvoices(accountId, page, perPage, undefined, invoiceTypes.join(','), ArchiveStatus.ENABLED, undefined, searchValues)).data;
+    return (await payingApi().getInvoices(accountId, page, perPage, undefined, mappedInvoiceTypes, ArchiveStatus.ENABLED, undefined, searchValues)).data;
   },
   getOne: async function (invoiceId: string): Promise<any> {
     const { accountId } = getCached.userInfo();
