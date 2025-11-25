23659a3af35bc9b52f7aa42cdb05e0b64e111057
RAMANANTSIRESY Amour Bien Aimé <95756837+AmourRamanantsiresy@users.noreply.github.com>
2025-11-25 15:47:37 +0300

    chore: remove 5cm precision restriction

--- src/operations/annotator/utils/analyse-roof-button.tsx
+++ src/operations/annotator/utils/analyse-roof-button.tsx
@@ -4,6 +4,7 @@ import { useAnnotatorComponentStore } from '@/common/store';
 import { clearPolygons } from '@/providers';
 import { Polygon } from '@bpartners/annotator-component';
 import { AreaPictureDetails } from '@bpartners/typescript-client';
+import { Tooltip } from '@mui/material';
 import { FC } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { AnnotatorFormState } from './annotations-info-form';
@@ -14,7 +15,7 @@ interface AnalyseRoofButtonProps extends Omit<BPButtonTemplateProps, 'label'> {
 }
 
 export const AnalyseRoofButton: FC<AnalyseRoofButtonProps> = ({ polygons, areaPicture, disabled, ...props }) => {
-  const { thereIsRoofPolygon } = useAnnotatorComponentStore();
+  const { thereIsRoofPolygon, areaPictureDetails } = useAnnotatorComponentStore();
   const annotatorFormState = useFormContext<AnnotatorFormState>();
   const handleSuccess = () => {
     annotatorFormState.setValue('polygons', [], { shouldDirty: true });
@@ -25,14 +26,27 @@ export const AnalyseRoofButton: FC<AnalyseRoofButtonProps> = ({ polygons, areaPi
 
   const handleClick = () => processDetection();
 
-  return (
+  const isPrecisionLevelInCmCorrect = areaPictureDetails?.actualLayer?.precisionLevelInCm === 5;
+
+  return isPrecisionLevelInCmCorrect ? (
     <BPButton
       {...props}
       className='analyse-roof-button'
       label='bp.action.process_detection'
-      disabled={disabled || !thereIsRoofPolygon}
+      disabled={disabled || !thereIsRoofPolygon || !isPrecisionLevelInCmCorrect}
       onClick={handleClick}
       isLoading={isProcessing}
     />
+  ) : (
+    <Tooltip title='L’image actuelle n’a pas une précision de 5 cm, donc l’analyse ne peut pas être lancée.'>
+      <BPButton
+        {...props}
+        className='analyse-roof-button'
+        label='bp.action.process_detection'
+        disabled={disabled || !thereIsRoofPolygon || !isPrecisionLevelInCmCorrect}
+        onClick={handleClick}
+        isLoading={isProcessing}
+      />
+    </Tooltip>
   );
 };
