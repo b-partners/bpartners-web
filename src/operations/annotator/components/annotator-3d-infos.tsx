9ff55a1baa8e9b792cc95a5bf37787afeb09566a
kirioka <hei.amour.2@gmail.com>
2025-11-25 16:53:42 +0300

    chore: show specific wall size

--- src/operations/annotator/components/annotator-3d-infos.tsx
+++ src/operations/annotator/components/annotator-3d-infos.tsx
@@ -1,10 +1,13 @@
 import { useAnnotator3DStore } from '@/common/store';
 import { ExpandMore } from '@mui/icons-material';
 import { Accordion, AccordionDetails, AccordionSummary, Box, FormControlLabel, Switch, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { getDistance } from '../utils';
 import { MeasurementIn2D } from './annotator-measurement-2d';
 
 export const Annotator3DInfos = () => {
   const { shouldSelectSurface, setShouldSelectSurface, selectObject, selectedObject, selectedObjectInfo, setSelectedObjectInfo } = useAnnotator3DStore();
+  const [wallHeights, setWallHeights] = useState({ maxH: null, minH: null });
 
   const handleClick = () => {
     setShouldSelectSurface(!shouldSelectSurface);
@@ -19,6 +22,31 @@ export const Annotator3DInfos = () => {
   const type = cityObject?.geometry?.[0]?.semantics?.surfaces?.[selectedObjectInfo.boundaryIndex]?.type;
   const height = cityObject?.geometry?.[0]?.semantics?.surfaces?.[selectedObjectInfo.boundaryIndex]?.height_in_meters;
 
+  const isWall = type === 'WallSurface';
+
+  useEffect(() => {
+    if (isWall) {
+      const boundaryIndex = selectedObjectInfo.boundaryIndex;
+      const boundary = cityObject?.geometry?.[0]?.boundaries?.[boundaryIndex]?.[0];
+      const verticles = selectedObject?.object?.citymodel?.vertices || [];
+      const points3D = boundary.map((index: number) => verticles[index]);
+      points3D.push(points3D[0]);
+
+      const measurements = [];
+
+      for (let i = 1; i < points3D.length; i++) {
+        const prev = points3D[i - 1];
+        const current = points3D[i];
+        measurements.push(+(getDistance(prev, current) * 0.001).toFixed(2));
+      }
+
+      setWallHeights({
+        maxH: Math.max(...measurements),
+        minH: Math.min(...measurements),
+      });
+    }
+  }, [isWall, cityObject]);
+
   return (
     <Box>
       <Accordion defaultExpanded>
@@ -31,10 +59,6 @@ export const Annotator3DInfos = () => {
         <Accordion defaultExpanded>
           <AccordionSummary expandIcon={<ExpandMore />}>Informations</AccordionSummary>
           <AccordionDetails>
-            <Typography>
-              <strong>Type : </strong>
-              {shouldSelectSurface ? type : cityObject?.type}
-            </Typography>
             {area && shouldSelectSurface && (
               <Typography>
                 <strong>Surface rampant : </strong>
@@ -45,6 +69,18 @@ export const Annotator3DInfos = () => {
               <Typography>
                 <strong>Hauteur : </strong>
                 {height}m
+              </Typography>
+            )}
+            {isWall && wallHeights.maxH && (
+              <Typography>
+                <strong>Hauteur de faîtage : </strong>
+                {wallHeights.maxH}m
+              </Typography>
+            )}
+            {isWall && wallHeights.minH && (
+              <Typography>
+                <strong>Hauteur de gouttière : </strong>
+                {wallHeights.minH}m
               </Typography>
             )}
             {slope && shouldSelectSurface && (
