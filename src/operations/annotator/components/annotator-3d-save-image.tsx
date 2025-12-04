0d19801c578456679922c021f467363b3bdb2439
amourRamanantsiresy <hei.amour.2@gmail.com>
2025-12-04 16:12:59 +0300

    fix: pdf image

--- src/operations/annotator/components/annotator-3d-save-image.tsx
+++ src/operations/annotator/components/annotator-3d-save-image.tsx
@@ -1,32 +1,34 @@
 import { useAnnotator3DStore } from '@/common/store';
 import { useThree } from '@react-three/fiber';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { v4 } from 'uuid';
 
 export const Annotator3DSaveImage = (): null => {
-  const { gl, scene } = useThree();
-  const { setImageUrl } = useAnnotator3DStore();
+  const { gl } = useThree();
+  const { setImageUrl, imageUrl } = useAnnotator3DStore();
+  const imageBase64 = useRef<string>('');
+  const intervalId = useRef<any>(null);
+  const counter = useRef<number>(0);
 
   useEffect(() => {
-    setInterval(() => {
-      console.log(scene.children.length);
-    }, 200);
-  }, []);
-
-  useEffect(() => {
-    if (scene.children.length > 0) {
+    intervalId.current = setInterval(() => {
       const dataUrl = gl.domElement.toDataURL('image/png');
+
+      if (imageBase64.current.length === 0) imageBase64.current = dataUrl;
+
+      imageBase64.current = dataUrl;
       const [header, base64] = dataUrl.split(',');
-
-      console.log(dataUrl);
-
       const mime = header.match(/:(.*?);/)?.[1] || 'image/png';
       const bytes = Uint8Array.from(atob(base64), c => c.charCodeAt(0));
       const file = new File([bytes], `${v4()}.png`, { type: mime });
-
       setImageUrl(file);
-    }
-  }, [scene.children.length]);
+      counter.current++;
+
+      if ((!imageUrl || imageUrl.size === 0) && counter.current >= 10) {
+        clearInterval(intervalId.current);
+      }
+    }, 200);
+  }, [imageUrl]);
 
   return null;
 };
