b599bdb9b5d573b24e380dc373aa2c0fb7e5c2e1
RAMANANTSIRESY Amour Bien Aimé <95756837+AmourRamanantsiresy@users.noreply.github.com>
2026-01-29 14:00:05 +0300

    chore: add last updated date on image source

--- src/operations/prospects/ProspectsList.jsx
+++ src/operations/prospects/ProspectsList.jsx
@@ -111,6 +111,7 @@ export const ProspectDialogProvider = ({ ComponentChild, address }) => {
           if (notSupportedPattern.test(err.message)) errorMessage = "La zone contenant cette adresse n'est pas encore supporté.";
           if (err.message.includes('Roof analysis consumption ') && err.message.includes(' limit exceeded for free trial period for User.id='))
             errorMessage = 'La limite des analyses gratuites a été atteinte.';
+          if ((err.message = 'PNEO data is not available yet on this area')) errorMessage = "Cette zone n'est pas disponible à 30cm";
 
           openDialog(
             <>
