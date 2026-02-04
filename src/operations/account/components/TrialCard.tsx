a92ec2d3e4ed9718e4f26b1905d01cc4d21072ca
amourRamanantsiresy <hei.amour.2@gmail.com>
2026-02-04 17:01:54 +0300

    fix: label on subscription free trial and subscribers

--- src/operations/account/components/TrialCard.tsx
+++ src/operations/account/components/TrialCard.tsx
@@ -1,9 +1,12 @@
+import { UserSubscription, UserSubscriptionStatus } from '@bpartners/typescript-client';
 import { Card, CardContent, Typography } from '@mui/material';
 import { useRecordContext } from 'react-admin';
 
 export const TrialCard = () => {
   const record = useRecordContext();
-  const subscription = record?.user?.subscription;
+  const subscription: UserSubscription = record?.user?.subscription;
+
+  const isFreeTrial = subscription?.status === UserSubscriptionStatus.FREE_TRIAL;
 
   return (
     <Card className='card card-trial'>
@@ -11,9 +14,13 @@ export const TrialCard = () => {
         <Typography className='section-title'>Période d’essai</Typography>
         {subscription?.status === 'ACTIVE' ? (
           <>
-            <Typography className='trial-desciption'>Vous bénéficiez actuellement d’une période d’essai gratuite.</Typography>
-            <Typography className='trial-start'>Début de la période d’essai : {subscription.start}</Typography>
-            <Typography className='trial-end'>Fin de la période d’essai : {subscription.end}</Typography>
+            {isFreeTrial && <Typography className='trial-desciption'>Vous bénéficiez actuellement d’une période d’essai gratuite.</Typography>}
+            <Typography className='trial-start'>
+              {isFreeTrial ? 'Début de la période d’essai' : 'Début de votre abonnement'} : {new Date(subscription.start).toLocaleDateString()}
+            </Typography>
+            <Typography className='trial-end'>
+              {isFreeTrial ? 'Fin de la période d’essai' : 'Fin de votre abonnement'} : {new Date(subscription.end).toLocaleDateString()}
+            </Typography>
           </>
         ) : (
           <Typography className='not-try'>Pas de période d’essai en cours.</Typography>
