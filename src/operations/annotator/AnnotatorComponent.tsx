a43bf0b65e34164417a02286284597ff6495a3b7
amourRamanantsiresy <hei.amour.2@gmail.com>
2026-03-26 11:20:51 +0300

    refactor: remove unused imports

--- src/operations/annotator/AnnotatorComponent.tsx
+++ src/operations/annotator/AnnotatorComponent.tsx
@@ -6,7 +6,7 @@ import { useDialog } from '@/common/store/dialog';
 import { getUrlParams, UrlParams } from '@/common/utils';
 import { clearPolygons } from '@/providers';
 import { AnnotatorCanvas, Polygon } from '@bpartners/annotator-component';
-import { AreaPictureDetails, ShiftDirection } from '@bpartners/typescript-client';
+import { ShiftDirection } from '@bpartners/typescript-client';
 import { Box, Stack, SxProps, Typography } from '@mui/material';
 import { Dispatch, FC, SetStateAction, useEffect } from 'react';
 import { degradationLevels } from '../prospects/constants';
