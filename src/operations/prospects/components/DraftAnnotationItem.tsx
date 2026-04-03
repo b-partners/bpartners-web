8b36c0497cc9c19eb2abd64ae26d425471f6e585
RAMANANTSIRESY Amour Bien Aimé <95756837+AmourRamanantsiresy@users.noreply.github.com>
2026-04-03 16:19:26 +0300

    chore: move draft annotations to my project

--- src/operations/prospects/components/DraftAnnotationItem.tsx
+++ src/operations/prospects/components/DraftAnnotationItem.tsx
@@ -15,10 +15,6 @@ import { CardViewField } from './CardViewField';
 
 const DRAFT_ANNOTATION_ITEM_SX: SxProps = {
   width: '100%',
-  maxWidth: {
-    lg: '400px',
-    sm: '100%',
-  },
   px: 1,
   pb: 1,
   gap: 2,
