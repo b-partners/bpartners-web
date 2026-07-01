325942aa2a2731cb4520a0b2305e74dae8fc6f2c
amourRamanantsiresy <hei.amour.2@gmail.com>
2026-07-01 16:59:50 +0300

    test: annotator it 1

---
+++ src/__tests__/it/account.it.cy copy 2.ts
@@ -0,0 +1,26 @@
+describe('Account', () => {
+  beforeEach(() => {
+    cy.realCognitoLogin();
+  });
+
+  it('is displayed on login', () => {
+    cy.e2eLogin();
+
+    cy.get('[name="account"]').click();
+    cy.get('[name="account"]').click();
+
+    cy.contains('NUMER');
+
+    cy.contains('InformationAndCommunication');
+    cy.contains('6000,00 €');
+    cy.contains('899067258');
+    cy.contains('FONTENAY-SOUS-BOIS');
+    cy.contains('FRA');
+    cy.contains('6 RUE PAUL LANGEVIN');
+    cy.contains('92002');
+
+    cy.contains('Fonenantsoa');
+    cy.contains('Maurica Andrianampoizinimaro');
+    cy.contains('+33648492113');
+  });
+});
