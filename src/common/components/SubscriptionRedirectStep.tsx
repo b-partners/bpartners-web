6287fb1e1551f1d6f8074d5608de240534ef5ff0
Ryan Andriamahery <ryan@hei.school>
2026-08-06 14:03:03 +0300

    fix: include subscription consent step and redirection page in subscription workflow

---
+++ src/common/components/SubscriptionRedirectStep.tsx
@@ -0,0 +1,47 @@
+import { cache } from '@/providers';
+import ArrowForwardRoundedIcon from '@mui/icons-material/ArrowForwardRounded';
+import { Box, DialogContent, Typography } from '@mui/material';
+import { FC, useEffect } from 'react';
+import { Redirect } from '../utils';
+import { SubscriptionRedirectStyle } from './style';
+
+const REDIRECT_DELAY_MS = 2500;
+
+const isStripeUrl = (url: string) => {
+  try {
+    return /(^|\.)stripe\.com$/i.test(new URL(url).hostname);
+  } catch {
+    return false;
+  }
+};
+
+interface SubscriptionRedirectStepProps {
+  redirectionUrl: string;
+}
+
+export const SubscriptionRedirectStep: FC<SubscriptionRedirectStepProps> = ({ redirectionUrl }) => {
+  const stripe = isStripeUrl(redirectionUrl);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      cache.whoami(undefined);
+      cache.user(undefined);
+      Redirect.toURL(redirectionUrl);
+    }, REDIRECT_DELAY_MS);
+    return () => clearTimeout(timer);
+  }, [redirectionUrl]);
+
+  return (
+    <DialogContent>
+      <Box sx={SubscriptionRedirectStyle}>
+        <Box className='redirect-arrow'>
+          <ArrowForwardRoundedIcon />
+        </Box>
+        <Typography className='redirect-title'>
+          {stripe ? 'Vous allez être redirigé vers Stripe pour souscrire à l’abonnement' : 'Votre abonnement est en cours d’enregistrement'}
+        </Typography>
+        <Typography className='redirect-subtitle'>Merci de patienter, vous allez être redirigé automatiquement.</Typography>
+      </Box>
+    </DialogContent>
+  );
+};
