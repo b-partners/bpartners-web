1981260ae7318e150bf9534261a49ad1100d7549
Ryan Andriamahery <ryan@hei.school>
2026-08-20 22:52:04 +0300

    feat(account): read billingInterval to describe the real payment mode

--- src/operations/account/queries/subscription-plans-query.ts
+++ src/operations/account/queries/subscription-plans-query.ts
@@ -5,8 +5,12 @@ import { useQuery } from '@tanstack/react-query';
 const toDisplayablePlans = (plans: SubscriptionPlan[]) =>
   plans.filter(({ isDeprecated }) => !isDeprecated).sort((a, b) => (a.displayPosition ?? Infinity) - (b.displayPosition ?? Infinity));
 
-export const useGetSubscriptionPlans = () => {
-  const { data = [], isLoading, isError } = useQuery({ queryKey: ['SubscriptionPlansQuery'], queryFn: getSubscriptionPlans, select: toDisplayablePlans });
+export const useGetSubscriptionPlans = (enabled = true) => {
+  const {
+    data = [],
+    isLoading,
+    isError,
+  } = useQuery({ queryKey: ['SubscriptionPlansQuery'], queryFn: getSubscriptionPlans, select: toDisplayablePlans, enabled });
   return {
     plans: data,
     isPlansLoading: isLoading,
